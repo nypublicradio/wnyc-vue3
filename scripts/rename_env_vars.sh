31820c63a17d4f2a2e6cb389cb345cb7ab489ed2
Wilfredo Colon <wcolon@nypublicradio.org>
2023-08-03 15:35:22 -0400

    Fixes to rename env vars

--- scripts/rename_env_vars.sh
+++ scripts/rename_env_vars.sh
@@ -1,4 +1,14 @@
-#!/bin/bash -x
+#!/bin/bash
+# Example usage
+# ENV="demo"
+# DEMO_VARNAME="Demo Value with spaces and special characters: * ! @"
+# PROD_VARNAME="Production Value"
+# # Run the script
+# ./rename_env_variables.sh
+
+# Now, the variables will be renamed to VARNAME based on the ENV value
+# echo "$VARNAME"         # Output: "Demo Value with spaces and special characters: * ! @"
+# echo "$PROD_VARNAME"    # Output: "Production Value"
 
 env_var_prefix=""
 
@@ -9,8 +19,11 @@ case "${ENV}" in
        exit 1 ;;
 esac
 
+# Loop through all environment variables
 while IFS= read -r env_var; do
     new_env_var="${env_var#${env_var_prefix}}"   # Remove the prefix
-    eval "${new_env_var}=\${${env_var}}"
-    echo export "${new_env_var}" >> local-env
+    value="${!env_var}"                         # Get the value of the original variable
+    quoted_value=$(printf "%q" "${value}")      # Quote the value to handle special characters
+    eval "${new_env_var}=${quoted_value}"       # Assign the quoted value to the renamed variable
+    export "${new_env_var}"
 done < <(printenv | grep "^${env_var_prefix}")
