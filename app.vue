41b014b5b570bc86e9aa95099f95b244cde8fb4a
Thomas Bono <thomasbono.com@gmail.com>
2023-08-07 14:02:44 -0400

    ready to push

--- app.vue
+++ app.vue
@@ -156,7 +156,7 @@ onMounted(async () => {
 
   // if APP then add listeners
   if (isApp.value) {
-    //addListeners()
+    addListeners()
     //checkAppLaunchUrl()
   }
 
@@ -207,6 +207,56 @@ useHead({
 </script>
 
 <template>
+  <Html lang="en">
+    <Head>
+      <Link rel="canonical" :href="`https://wnyc.org${route.path}`" />
+      <Link rel="stylesheet" :href="config.public.HTL_CSS" type="text/css" />
+      <Title>
+        WNYC | New York Public Radio, Podcasts, Live Streaming Radio, News
+      </Title>
+      <Meta
+        name="description"
+        content="WNYC is America's most listened-to public radio station and the producer of award-winning programs and podcasts like Radiolab, On the Media, and The Brian Lehrer Show."
+      />
+      <Meta
+        name="keywords"
+        content="wnyc, podcasts, npr, new york, WNYC Studios, arts, culture, classical, music, news, public, radio"
+      />
+      <Meta
+        name="og:site_name"
+        content="WNYC | New York Public Radio, Podcasts, Live Streaming Radio, News"
+      />
+      <Meta name="og:type" content="website" />
+      <Meta name="og:url" :content="`https://www.wnyc.org${route.fullPath}`" />
+      <Meta
+        name="og:title"
+        content="WNYC | New York Public Radio, Podcasts, Live Streaming Radio, News"
+      />
+      <Meta
+        name="og:description"
+        content="WNYC is America's most listened-to public radio station and the producer of award-winning programs and podcasts like Radiolab, On the Media, and The Brian Lehrer Show."
+      />
+      <Meta
+        name="og:image"
+        content="https://media.wnyc.org/i/1200/1200/c/80/1/wnyc_square_logo.png"
+      />
+      <Meta name="og:image:alt" content="WNYC" />
+      <Meta name="og:image:width" content="1200" />
+      <Meta name="og:image:height" content="600" />
+      <Meta name="fb:app_id" content="151261804904925" />
+      <Meta name="twitter:card" content="summary_large_image" />
+      <Meta name="twitter:site" content="@radiolab" />
+      <Meta name="twitter:title" content="WNYC" />
+      <Meta
+        name="twitter:description"
+        content="WNYC | New York Public Radio, Podcasts, Live Streaming Radio, News"
+      />
+      <Meta
+        name="twitter:image"
+        content="https://media.wnyc.org/i/1200/1200/c/80/1/wnyc_square_logo.png"
+      />
+    </Head>
+  </Html>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import VSmartHeader from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VSmartHeader.vue'
 const route = useRoute()
-const config = useRuntimeConfig()
 useHead({
   bodyAttrs: {
     class: 'template-default',
@@ -11,59 +10,6 @@ useHead({
 
 <template>
   <div class="page" :class="[`${String(route.name)}`]">
-    <Html lang="en">
-      <Head>
-        <Link rel="canonical" :href="`https://wnyc.org${route.path}`" />
-        <Link rel="stylesheet" :href="config.public.HTL_CSS" type="text/css" />
-        <Title>
-          WNYC | New York Public Radio, Podcasts, Live Streaming Radio, News
-        </Title>
-        <Meta
-          name="description"
-          content="WNYC is America's most listened-to public radio station and the producer of award-winning programs and podcasts like Radiolab, On the Media, and The Brian Lehrer Show."
-        />
-        <Meta
-          name="keywords"
-          content="wnyc, podcasts, npr, new york, WNYC Studios, arts, culture, classical, music, news, public, radio"
-        />
-        <Meta
-          name="og:site_name"
-          content="WNYC | New York Public Radio, Podcasts, Live Streaming Radio, News"
-        />
-        <Meta name="og:type" content="website" />
-        <Meta
-          name="og:url"
-          :content="`https://www.wnyc.org${route.fullPath}`"
-        />
-        <Meta
-          name="og:title"
-          content="WNYC | New York Public Radio, Podcasts, Live Streaming Radio, News"
-        />
-        <Meta
-          name="og:description"
-          content="WNYC is America's most listened-to public radio station and the producer of award-winning programs and podcasts like Radiolab, On the Media, and The Brian Lehrer Show."
-        />
-        <Meta
-          name="og:image"
-          content="https://media.wnyc.org/i/1200/1200/c/80/1/wnyc_square_logo.png"
-        />
-        <Meta name="og:image:alt" content="WNYC" />
-        <Meta name="og:image:width" content="1200" />
-        <Meta name="og:image:height" content="600" />
-        <Meta name="fb:app_id" content="151261804904925" />
-        <Meta name="twitter:card" content="summary_large_image" />
-        <Meta name="twitter:site" content="@radiolab" />
-        <Meta name="twitter:title" content="WNYC" />
-        <Meta
-          name="twitter:description"
-          content="WNYC | New York Public Radio, Podcasts, Live Streaming Radio, News"
-        />
-        <Meta
-          name="twitter:image"
-          content="https://media.wnyc.org/i/1200/1200/c/80/1/wnyc_square_logo.png"
-        />
-      </Head>
-    </Html>
     <div class="top-safe-cover" />
     <header>
       <VSmartHeader :hero-buffer="800" :resume-delay="0">
