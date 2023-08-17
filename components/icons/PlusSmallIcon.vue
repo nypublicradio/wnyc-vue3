379b36f4e9d3588a95fc22e8874fd5b3317f98c1
Kelly Suitor <kelly.suitor@gmail.com>
2023-08-17 18:15:50 -0400

    Add 18 SVG icon components to the WNYC App

---
+++ components/icons/PlusSmallIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    role="img"
+    aria-label="plussmall icon"
+    class="plussmall-icon o-icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+  >
+    <title>Plus Small</title>
+    <path
+      d="M18.25 9H11V1.75C11 1.34 10.55 1 10 1C9.45 1 9 1.34 9 1.75V9H1.75C1.34 9 1 9.45 1 10C1 10.55 1.34 11 1.75 11H9V18.25C9 18.66 9.45 19 10 19C10.55 19 11 18.66 11 18.25V11H18.25C18.66 11 19 10.55 19 10C19 9.45 18.66 9 18.25 9Z"
+      fill="currentColor"
+    />
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+.plussmall-icon {
+  height: 20px;
+  width: 20px;
+}
+</style>
---
+++ components/icons/SuccessSmallIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg
+    role="img"
+    aria-label="successsmall icon"
+    class="successsmall-icon o-icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+  >
+    <title>Success Small</title>
+    <path
+      d="M10 19.25C4.9 19.25 0.75 15.1 0.75 10C0.75 4.9 4.9 0.75 10 0.75C15.1 0.75 19.25 4.9 19.25 10C19.25 15.1 15.1 19.25 10 19.25ZM10 2.25C5.73 2.25 2.25 5.73 2.25 10C2.25 14.27 5.73 17.75 10 17.75C14.27 17.75 17.75 14.27 17.75 10C17.75 5.73 14.27 2.25 10 2.25Z"
+      fill="currentColor"
+    />
+    <path
+      d="M8.58 13.64L7.98 13.14L5.71 10.94C5.41 10.65 5.41 10.18 5.69 9.88C5.98 9.58 6.45 9.58 6.75 9.86L8.52 11.58L13.23 6.87C13.52 6.58 14 6.58 14.29 6.87C14.58 7.16 14.58 7.64 14.29 7.93L8.58 13.65V13.64Z"
+      fill="currentColor"
+    />
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+.successsmall-icon {
+  height: 20px;
+  width: 20px;
+}
+</style>
