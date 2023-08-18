607d269ab8d78b012914e5d33dcf97d0f3162d8f
Wilfredo Colon <wcolon@nypublicradio.org>
2023-08-18 12:43:30 -0400

    Pulled in bono branch and fixed android gradle

--- app.vue
+++ app.vue
@@ -28,51 +28,6 @@ const fcmToken = ref('')
 //const nNotification = ref(null)
 // const appLaunchUrl = ref(null)
 
-//New Relic Instrumentation
-var appToken;
-if (Capacitor.getPlatform() === 'ios') {
-  appToken = 'AA731b117edcff278d3a204187b46ab3d347a16888-NRMA'
-} else {
-  appToken = 'AA8cda4e9935230376ef2dad8356c3b9bada1365df-NRMA'
-}
-
-let agentConfig: AgentConfiguration = {
-  // Android specific option
-  // Optional: Enable or disable collection of event data.
-  analyticsEventEnabled: true,
-
-  // iOS specific option
-  // Optional: Enable/Disable automatic instrumentation of WebViews.
-  webViewInstrumentation: true,
-
-  // Optional: Enable or disable crash reporting.
-  crashReportingEnabled: true,
-
-  // Optional: Enable or disable interaction tracing. Trace instrumentation still occurs, but no traces are harvested. This will disable default and custom interactions.
-  interactionTracingEnabled: true,
-
-  // Optional: Enable or disable reporting successful HTTP requests to the MobileRequest event type.
-  networkRequestEnabled: true,
-
-  // Optional: Enable or disable reporting network and HTTP request errors to the MobileRequestError event type.
-  networkErrorRequestEnabled: true,
-
-  // Optional: Enable or disable capture of HTTP response bodies for HTTP error traces, and MobileRequestError events.
-  httpResponseBodyCaptureEnabled: true,
-
-  // Optional: Enable or disable agent logging.
-  loggingEnabled: true,
-
-  // Optional: Specifies the log level. Omit this field for the default log level.
-  // Options include: ERROR (least verbose), WARNING, INFO, VERBOSE, AUDIT (most verbose).
-  logLevel: NREnums.LogLevel.INFO,
-
-  // Optional: Enable or disable sending JS console logs to New Relic.
-  sendConsoleEvents: true
-}
-
-NewRelicCapacitorPlugin.start({ appKey: appToken, agentConfiguration: agentConfig })
-
 isApp.value = Capacitor.getPlatform() !== 'web'
 
 useHead({
