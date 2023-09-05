6ea5027356c705d8fe396b872a1f75addfb93140
Thomas Bono <bonomite@pop-os.localdomain>
2023-09-05 13:22:06 -0400

    merge 66 to main

---
+++ components/LatestNewsUpdates.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, computed, onMounted } from 'vue'
+
+const props = defineProps({
+  //   propVar: {
+  //     type: Boolean,
+  //     default: false,
+  //   },
+})
+
+//const emit = defineEmits(["change", "click"]);
+
+// lifecycle hooks
+onMounted(() => {})
+</script>
+
+<template>
+  <div>
+    <div class="latest-news-updates">Here</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.latest-news-updates {
+}
+</style>
--- components/TopStories.vue
+++ components/TopStories.vue
@@ -1,7 +1,7 @@
 <script setup>
 import VCard from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VCard.vue'
 import VByline from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VByline.vue'
-import { trackClickEvent } from '~/utilities/helpers'
+import { trackClickEvent, howLongAgo } from '~/utilities/helpers'
 // get the navigation data from Aviary
 const config = useRuntimeConfig()
 const { data: articles } = await useFetch(config.public.STORIES_API)
@@ -46,20 +46,19 @@ const normalizeAuthor = (author) => {
 
 <template>
   <div v-if="articles" class="top-stories">
-    <div v-for="(article, index) in articles.items" :key="index" class="mb-6">
+    <div v-for="(article, index) in articles.items" :key="index" class="mb-4">
+      <!--       <pre>{{ article }}</pre> -->
       <VCard
         :src="getImageUrl(article)"
         :title="article.title"
-        :blurb="article.description"
         :loading="index > 1 ? 'lazy' : 'eager'"
         :link="getArticleLink(article)"
         :maxWidth="article.listingImage?.width"
         :maxHeight="article.listingImage?.height"
         :sponsored="article.sponsoredContent"
-        :width="318"
-        :height="212"
-        :vertical="true"
-        reverse
+        :width="116"
+        :height="116"
+        :ratio="[1, 1]"
         @title-click="
           trackClickEvent(
             'Click Tracking - Top Story',
@@ -78,6 +77,7 @@ const normalizeAuthor = (author) => {
         <template #belowBlurb>
           <div class="article-metadata">
             <VByline
+              prefix=""
               :authors="article.related_authors?.map(normalizeAuthor)"
               @name-click="
                 trackClickEvent(
@@ -93,7 +93,11 @@ const normalizeAuthor = (author) => {
                   $event.url
                 )
               "
-            />
+            >
+              <template #afterNames>
+                <span>| {{ howLongAgo(article.meta.first_published_at) }}</span>
+              </template>
+            </VByline>
           </div>
         </template>
       </VCard>
@@ -101,13 +105,32 @@ const normalizeAuthor = (author) => {
   </div>
 </template>
 
-<style lang="scss" scoped>
-.top-stories {
-  border-top: 2px solid rgba(map-get($colors, 'background'), 0.2);
-}
-</style>
 <style lang="scss">
-.article-metadata .v-byline .flexible-link {
-  border-bottom: none;
+.top-stories {
+  .v-card {
+    .card-details {
+      flex: 1;
+      align-self: stretch !important;
+      justify-content: space-between;
+    }
+    .card-title-title {
+      font-size: 0.906rem;
+      line-height: 1.25rem;
+      font-weight: 700;
+      @include truncate();
+      @include t4lines();
+    }
+    .slot-below-blurb {
+      font-size: 0.813rem;
+      font-weight: 400;
+      .flexible-link {
+        color: initial;
+        text-decoration: none;
+      }
+      .v-byline {
+        gap: 5px;
+      }
+    }
+  }
 }
 </style>
