5ab3354db53caf410ed9627aa0fa4c052ac125c7
Thomas Bono <bonomite@pop-os.localdomain>
2023-09-05 13:30:23 -0400

    deepsource 2

--- components/LatestNewsUpdates.vue
+++ components/LatestNewsUpdates.vue
@@ -1,12 +1,5 @@
-<script setup></script>
-
 <template>
   <div>
     <div class="latest-news-updates">In Progress</div>
   </div>
 </template>
-
-<style lang="scss" scoped>
-.latest-news-updates {
-}
-</style>
