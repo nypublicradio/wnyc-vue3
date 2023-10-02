3586737fb091ca0508b375360dca93fed0151973
Kim LaRocca <kim@kimlarocca.com>
2023-10-02 11:43:00 -0400

    episode details page so far + dummy data

---
+++ pages/episode/[slug].vue
@@ -0,0 +1,82 @@
+<script setup>
+import VImage from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VImage.vue'
+import { trackClickEvent, whenTime } from '~/utilities/helpers'
+
+// TO DO - replace dummy data with BFF data
+import episodeData from './episode-data.json'
+
+// navigate back to home and track it
+const backHome = () => {
+  trackClickEvent('episode', 'episode page', 'back home button')
+  navigateTo(`/shows/${episodeData.show}`)
+}
+</script>
+
+<template>
+  <div v-if="episodeData" class="episode-page">
+    <section class="">
+      <div class="flex">
+        <Button
+          class="back-btn text-color -ml-3"
+          icon="pi pi-chevron-left"
+          rounded
+          text
+          severity="secondary"
+          aria-label="back to previous page"
+          @click="backHome"
+        />
+        <h1>{{ episodeData?.show }}</h1>
+      </div>
+    </section>
+    <v-image
+      v-if="episodeData?.image"
+      :src="episodeData?.image.url"
+      :ratio="[3, 2]"
+      :alt="episodeData?.image.alt"
+      class="episode-page-image mb-2"
+    />
+    <section>
+      <p class="episode-page-date mb-1">{{ whenTime(episodeData) }}</p>
+      <h1 class="mb-4 alt">{{ episodeData.title }}</h1>
+      <div class="episode-page-body" v-html="episodeData.body" />
+    </section>
+    <section v-if="episodeData.transcript">
+      <h3 class="mb-4">Transcript</h3>
+      <div class="episode-page-transcript" v-html="episodeData.transcript" />
+    </section>
+  </div>
+</template>
+
+<style lang="scss">
+.episode-page .episode-page-image {
+  width: 100%;
+  max-height: 333.33px;
+  aspect-ratio: 3/2;
+  object-fit: cover;
+}
+
+.episode-page .episode-page-date {
+  font-size: var(--font-size-4);
+  font-weight: var(--font-weight-400);
+  line-height: var(--font-size-6);
+  color: var(--text-color);
+  text-decoration: none;
+  opacity: 70%;
+}
+
+.episode-page h1.alt {
+  font-family: var(--font-family-header);
+  font-size: var(--font-size-8);
+  font-weight: var(--font-weight-400);
+  line-height: var(--font-size-10);
+}
+
+.episode-page .star-icon {
+  height: 28px;
+  width: 28px;
+}
+
+.episode-page-body hr {
+  margin: 1.5rem 0;
+}
+</style>
