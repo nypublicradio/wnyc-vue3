6f600a790f79eec1956008067db2cb0c6f4af005
Wilfredo Colon <wcolon@nypublicradio.org>
2023-10-19 05:42:15 -0400

    Adding featured shows to shows endpoint

--- server/api/shows.ts
+++ server/api/shows.ts
@@ -22,10 +22,14 @@ const featuredShows = async () => {
     try {
         const option = {
             method: 'GET',
-            url: `${config.public.PUBLISHER_BASE_API}v1/list/featured-shows/`,
+            url: config.public.FEATURED_SHOWS,
+            params: {
+                discover_station: 'wnyc-vue3-app-featured',
+                api_key: 'spotlight',
+            }
         };
         const res = await axios(option);
-        return humps.camelizeKeys(res.data).data;
+        return humps.camelizeKeys(res.data);
     } catch (e) {
         //console.log(e);
     }
@@ -34,10 +38,9 @@ const featuredShows = async () => {
 
 export default defineEventHandler(async (event) => {
     const allShowsData = await allShows();
-    //TODO: Uncomment this once the API is ready
-    //const featuredShowsData = await featuredShows();
+    const featuredShowsData = await featuredShows();
     return {
-            all: allShowsData
-            //featuredShows: featuredShowsData
+            all: allShowsData,
+            featuredShows: featuredShowsData
     }
 });
