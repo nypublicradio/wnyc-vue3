8d63134eb309d9f10214110268d53e5074f66a41
bonomite <thomasbono.com@gmail.com>
2023-10-23 17:15:16 -0400

    getting started, considering making the horz scroll component a scrollable tab prime vue component

--- pages/saved.vue
+++ pages/saved.vue
@@ -1,7 +1,16 @@
 <template>
-  <section class="saved-page">
-    <NuxtLink to="/home">GO back home</NuxtLink>
-    <br />
-    Saved Page
-  </section>
+  <div class="saved-page">
+    <section class="flex align-items-center justify-content-between">
+      <h1>Saved</h1>
+      <Button
+        class="-mr-3 text-sm"
+        label="Add"
+        text
+        plain
+        icon="pi pi-plus"
+        iconPos="right"
+        size="large"
+      ></Button>
+    </section>
+  </div>
 </template>
