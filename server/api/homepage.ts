bdfc85c8243cf9c18026b7c29d916fd1175934ba
Wilfredo Colon <wcolon@nypublicradio.org>
2023-10-30 15:56:21 -0400

    NYC NOW during business hours

--- server/api/homepage.ts
+++ server/api/homepage.ts
@@ -1,6 +1,7 @@
 const config = useRuntimeConfig()
 import axios from 'axios'
 import humps from 'humps'
+import { normalizeWagtailPage, normalizePublisherPage } from '~/composables/data/articlePages'
 
 const GOTHAMISTDOTCOM = 'https://gothamist.com/'
 
@@ -60,6 +61,39 @@ const getNationalNewscast = async () => {
 		////console.log(e);
 	}
 }
+
+// Get NYC-NOW newscast from the WNYC API
+const getNYCNowNewscast = async () => {
+	try {
+		const options = {
+			method: 'GET',
+			url: `${config.public.PUBLISHER_BASE_API}v3/story/`,
+			params: {
+				'channel': 'nyc-now-podcast',
+				'ordering': '-newsdate',
+				'page_size': '1',
+			},
+		};
+		const res = await axios(options);
+		const resData = humps.camelizeKeys(res.data).data[0];
+		resData.attributes.file = resData.attributes.audio;
+		resData.attributes.image = 'https://media.wnyc.org/i/%s/%s/%s/%s/2023/04/NYNOW_WNYC_LOGO_HEX_1400PX.png';
+		//Fetch the mp3 Content-Length and calculate the duration in seconds
+		const mp3Res = await axios(resData.attributes.audio);
+		const mp3Size = mp3Res.headers['content-length'];
+		// Calculate the duration in seconds not converting size into bits. 
+		// The bitrate is 128kps according to vlc and the file size is in bytes.
+		//Multiplying the file size by 8 and dividing by 128000 gives the same
+		//duration as dividing by 16000 and not multiplying the file
+		const duration = Math.round(mp3Size / 16000) * 1000;
+		resData.attributes.duration = duration;
+		resData.attributes.cardTitle = 'Latest Headlines';
+		return resData.attributes;
+	} catch (e) {
+		//console.log(e);
+	}
+}
+
 const getNavigation = async () => {
 	try {
 		const options = {
@@ -174,7 +208,6 @@ const getWNYCTopStories = async () => {
 				article.cmsSource = 'publisher';
 				article.SortDate = article.attributes.publishAt;
 				return article;
-
 			});
 			return articles;
 		} else {
@@ -226,11 +259,22 @@ export default defineEventHandler(async (event) => {
 	const aviary = await getGothamistTopStories();
 	const publisher = await getWNYCTopStories();
 	const bucket = await getMiddleBucket();
-	//const topStories = mergeArticles(aviary, publisher);
-	const local_newscast = await getLocalNewscast();
+	const topStories = mergeArticles(aviary, publisher);
+	// WNYC NOW Newscast is only available on weekdays between 7am and 7pm
+	// If it is not available, use the local newscast instead.
+	const requestTime = new Date();
+	const day = requestTime.getDay();
+	const hour = requestTime.getHours();
+	let local_newscast;
+	if (day > 0 && day < 6 && hour > 6 && hour < 19) {
+		local_newscast = await getNYCNowNewscast();
+	} else {
+		local_newscast = await getLocalNewscast();
+	}
 	const national_newscast = await getNationalNewscast();
+
 	return {
-		top_stories: aviary,
+		top_stories: topStories,
 		middle_bucket: bucket,
 		local_newscast: local_newscast,
 		national_newscast: national_newscast,
