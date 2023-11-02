01123cdd34b544cde6da463bee1574852e1cf620
Wilfredo Colon <wcolon@nypublicradio.org>
2023-11-02 12:16:15 -0400

    strip bad formatting

--- server/api/story/[cmsSource]/[storyId].ts
+++ server/api/story/[cmsSource]/[storyId].ts
@@ -1,19 +1,54 @@
-import axios from 'axios' import humps from 'humps' import {
-normalizeArticlePage} from '~/composables/data/articlePages' const config =
-useRuntimeConfig(); const getWagtailStoryData = async (id: string) => { try {
-const option = { method: 'GET', url:
-`${config.public.AVIARY_BASE_API}pages/${id}/`, }; const res = await
-axios(option); //return humps.camelizeKeys(res.data); return
-normalizeArticlePage(humps.camelizeKeys(res.data)); } catch (e) {
-//console.log(e); } }; const getPublisherStoryData = async (id: string) => { try
-{ const option = { method: 'GET', url:
-`${config.public.PUBLISHER_BASE_API}v3/story-pk/${id}/`, }; const res = await
-axios(option); return normalizeArticlePage(humps.camelizeKeys(res.data).data); }
-catch (e) { //console.log(e); } }; const getStoryData = async (id: string,
-cmsSource: string) => { switch (cmsSource) { case 'wagtail': return await
-getWagtailStoryData(id); case 'publisher': return await
-getPublisherStoryData(id); default: return null; }; }; // Get story data from
-CMS export default defineEventHandler(async (event) => { const id: string |
-undefined = event?.context?.params?.storyId; const cmsSource: string | undefined
-= event?.context?.params?.cmsSource; if (id && cmsSource) { const storyData =
-await getStoryData(id, cmsSource); return storyData; } });
+import axios from 'axios'
+import humps from 'humps'
+import { normalizeArticlePage} from '~/composables/data/articlePages'
+
+const config = useRuntimeConfig();
+
+const getWagtailStoryData = async (id: string) => {
+    try {
+        const option = {
+            method: 'GET',
+            url: `${config.public.AVIARY_BASE_API}pages/${id}/`,
+        };
+        const res = await axios(option);
+        //return humps.camelizeKeys(res.data);
+        return normalizeArticlePage(humps.camelizeKeys(res.data));
+    } catch (e) {
+        //console.log(e);
+    }
+};
+
+const getPublisherStoryData = async (id: string) => {
+    try {
+        const option = {
+            method: 'GET',
+            url: `${config.public.PUBLISHER_BASE_API}v3/story-pk/${id}/`,
+        };
+        const res = await axios(option);
+        return normalizeArticlePage(humps.camelizeKeys(res.data).data);
+    } catch (e) {
+        //console.log(e);
+    }
+};
+
+const getStoryData = async (id: string, cmsSource: string) => {
+    switch (cmsSource) {
+        case 'wagtail':
+            return await getWagtailStoryData(id);
+        case 'publisher':
+            return await getPublisherStoryData(id);
+        default:
+            return null;
+    };
+};
+
+// Get story data from CMS
+
+export default defineEventHandler(async (event) => {
+    const id: string | undefined = event?.context?.params?.storyId;
+    const cmsSource: string | undefined = event?.context?.params?.cmsSource;
+    if (id && cmsSource) {
+        const storyData = await getStoryData(id, cmsSource);
+        return storyData;
+    }
+});
