03286f952ee385e3b9a803e63633e1603adf8077
bonomite <thomasbono.com@gmail.com>
2023-11-05 10:59:07 -0500

    made and used Story component

--- components/TopStories.vue
+++ components/TopStories.vue
@@ -23,63 +23,7 @@ defineExpose({
   <div v-if="articles" class="top-stories">
     <div v-for="(article, index) in articles" :key="article.id" class="mb-4">
       <!--   <pre class="text-xs">{{ article }}</pre> -->
-      <VCard
-        v-ripple
-        class="p-ripple"
-        :src="
-          article.cmsSource === cmsSources.WAGTAIL
-            ? article.leadImage
-            : article.image?.template
-        "
-        :title="article.title"
-        :loading="index > 1 ? 'lazy' : 'eager'"
-        :maxWidth="article.leadImageMaxWidth"
-        :maxHeight="article.leadImageMaxHeight"
-        :sponsored="article.sponsoredContent"
-        :width="116"
-        :height="116"
-        :ratio="[1, 1]"
-        @click="
-          navigateTo({
-            path: `/story/${article.id}`,
-            query: {
-              src: article.cmsSource,
-            },
-          })
-        "
-        @title-click="
-          trackClickEvent(
-            'Click Tracking - Top Story',
-            'Article Card Headline',
-            $event
-          )
-        "
-        @image-click="
-          trackClickEvent(
-            'Click Tracking - Top Story',
-            'Article Card Image',
-            $event
-          )
-        "
-      >
-        <template #belowBlurb>
-          <div class="article-metadata pointer-events-none">
-            <PipeData
-              :hidePipe="
-                article.authors?.length == 0 || article.authors == undefined
-              "
-            >
-              <template #left>
-                <VByline prefix="" :authors="article.authors" isBlockLinks>
-                </VByline>
-              </template>
-              <template #right>
-                <span class="nobreak">{{ whenTime(article.meta) }}</span>
-              </template>
-            </PipeData>
-          </div>
-        </template>
-      </VCard>
+      <Story :article="article" :index="index" />
     </div>
   </div>
   <div v-else>
@@ -93,31 +37,5 @@ defineExpose({
 
 <style lang="scss">
 .top-stories {
-  .v-card {
-    cursor: pointer;
-    .card-details {
-      flex: 1;
-      align-self: stretch !important;
-      justify-content: space-between;
-    }
-    .card-title-title {
-      font-size: 0.906rem;
-      line-height: 1.25rem;
-      font-weight: 700;
-      @include truncate();
-      @include t4lines();
-    }
-    .slot-below-blurb {
-      font-size: 0.813rem;
-      font-weight: 400;
-      .flexible-link {
-        color: inherit;
-        text-decoration: none;
-      }
-      .v-byline {
-        gap: 0;
-      }
-    }
-  }
 }
 </style>
