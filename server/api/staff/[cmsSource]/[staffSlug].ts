87847216747b0ec74db55f4799d6b385ab9828e8
Wilfredo Colon <wcolon@nypublicradio.org>
2023-11-06 16:25:33 -0500

    Staff data and article with article count

--- server/api/staff/[cmsSource]/[staffSlug].ts
+++ server/api/staff/[cmsSource]/[staffSlug].ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import humps from 'humps'
+import type { ArticlePage } from '~~/composables/types/Page'
 import { normalizePublisherPage, normalizeWagtailPage } from '~/composables/data/articlePages'
 
 const config = useRuntimeConfig();
@@ -13,12 +14,40 @@ const config = useRuntimeConfig();
 //     }]
 // }
 
+const getWagtailStaffData = async (staffSlug: string, offset: number) => {
+    const options = {
+        method: 'GET',
+        url: `${config.public.AVIARY_BASE_API}pages/`,
+        params: {
+            author_slug: staffSlug,
+            type: 'news.ArticlePage',
+            fields: 'id,title,lead_asset,related_authors,publication_date,ancestry',
+            order: '-publication_date',
+            limit: 10,
+            offset: offset,
+        },
+    };
+    const res = await axios(options);
+    const author = res.data.items[0].related_authors.map((author: any) => {
+        return author;
+    });
+    const articles = res.data.items.map((item: ArticlePage) => normalizeWagtailPage(item));
+    return {
+        authorData: author,
+        articles,
+        count: res.data.meta.total_count,
+    }
+};
 
-const getStaffData = async (staffSlug: string, cmsSource: string) => {
+const getPublisherStaffData = async (staffSlug: string) => {
+    //todo: call publisher api to get staff data with article list
+};
+
+const getStaffData = async (staffSlug: string, cmsSource: string, offset: number) => {
 
     switch (cmsSource) {
         case 'wagtail':
-            return await getWagtailStaffData(staffSlug);
+            return await getWagtailStaffData(staffSlug,offset);
         case 'publisher':
             return await getPublisherStaffData(staffSlug);
         default:
@@ -31,8 +60,10 @@ const getStaffData = async (staffSlug: string, cmsSource: string) => {
 export default defineEventHandler(async (event) => {
     const staffSlug: string | undefined = event?.context?.params?.staffSlug;
     const cmsSource: string | undefined = event?.context?.params?.cmsSource;
+    // query params
+    const offset: number = event?.context?.query?.offset || 0;
     if (staffSlug && cmsSource) {
-        const storyData = await getStaffData(staffSlug, cmsSource);
+        const storyData = await getStaffData(staffSlug, cmsSource, offset);
         return storyData;
     }
 });
