700903b46152bcc4d3df3d8634af6968235f38a3
bonomite <thomasbono.com@gmail.com>
2023-11-07 13:25:48 -0500

    removed unused composable

--- composables/useLoadMoreArticles.ts
+++
@@ -1,7 +0,0 @@
-export default async function useLoadMoreArticles(options) {
-    const newArticles =  await findArticlePages(options).then(({ data }) =>
-        normalizeFindArticlePagesResponse(data)
-    )
-    useUpdateCommentCounts(newArticles)
-    return newArticles
-}
