997898e3182588e32f7f91a12660baba340f7c5f
Wilfredo Colon <wcolon@nypublicradio.org>
2023-11-08 13:21:42 -0500

    Removing server block from capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,10 +5,10 @@ const config: CapacitorConfig = {
   appName: 'wnyc-app-android',
   webDir: 'dist',
   backgroundColor: "#d4d4d4",
-  server: {
+/*   server: {
     androidScheme: 'https',
     url: "https://demo.wnyc.org",
-  },
+  }, */
   plugins: {
     CapacitorCookies: {
       enabled: true,
