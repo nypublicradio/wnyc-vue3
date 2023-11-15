f9cc4dea3eca021f391125e07c212e5691f9d56d
Wilfredo Colon <wcolon@nypublicradio.org>
2023-11-15 13:30:34 -0500

    Add estimated duration to episodes without it

--- server/api/show/[showslug].ts
+++ server/api/show/[showslug].ts
@@ -1,7 +1,27 @@
 import axios from 'axios'
 import humps from 'humps'
+import { fetchDuration } from '~/utilities/helpers'
 
 const config = useRuntimeConfig()
+
+/* const fetchDuration = async (url: string) => {
+    try{
+        const options = {
+            method: 'HEAD',
+            url: url,
+        };
+        const mp3Res = await axios(options);
+        const mp3Size = mp3Res.headers['content-length'];
+        // Calculate the duration in seconds not converting size into bits. 
+        // The bitrate is 128kps according to vlc and the file size is in bytes.
+        //Multiplying the file size by 8 and dividing by 128000 gives the same 
+        //duration as dividing by 16000 and not multiplying the file size by 8.
+        const duration: number = Math.round(mp3Size / 16000) * 1000;
+        return duration
+    } catch (e) {
+        //console.log(e);
+    }
+  } */
 
 const getEpisodes = async (slug: string, type: string, page?: string) => {
     try {
@@ -20,13 +40,19 @@ const getEpisodes = async (slug: string, type: string, page?: string) => {
         };
         const res = await axios(option);
         const cleanEpisodes = res.data.data.filter((episode: any) => {
-            // removeing episodes with no audio
+            // removing episodes with no audio
             if (Array.isArray(episode.attributes.audio)) {
                 return episode.attributes.audio[0] !== null
             } else {
                 return episode.attributes.audio !== null
             }                       
         })
+        for (let i = 0; i < cleanEpisodes.length; i++) {
+            if (!cleanEpisodes[i].attributes.estimatedDuration) {
+                const url: string = cleanEpisodes[i].attributes.audio
+                cleanEpisodes[i].attributes.estimatedDuration = await fetchDuration(url)
+            }
+        }
         //Passing meta and data separately to the client. Meta is to used for pagination
         return {
             data: humps.camelizeKeys(cleanEpisodes),
@@ -55,6 +81,7 @@ const getShow = async (slug: string) => {
 }
 
 export default defineEventHandler(async (event) => {
+    let res = event?.node?.res;
     //Fetching slug and type from the path params
     const slug: string | undefined = event?.context?.params?.showslug;
     //Fetching query params
@@ -64,11 +91,11 @@ export default defineEventHandler(async (event) => {
         // Get show details
         const show = await getShow(slug);
         const episodes = await getEpisodes(slug, show.type, page);
-
+        res.setHeader('Cache-Control', 'maxage=3600, stale-while-revalidate');
         return {
             show: show,
             episodes: episodes
         }
     }
     return null;
-});
+})
