6e54b8b355f4d6b8b19dd6b61d1879ceffa05b87
bonomite <thomasbono.com@gmail.com>
2023-12-08 18:15:51 -0500

    better errors to help figure out what is happening

--- server/api/browse/browse-topic/getTopicData.ts
+++ server/api/browse/browse-topic/getTopicData.ts
@@ -1,11 +1,16 @@
 
 import axios from 'axios'
 import humps from 'humps'
+//import [useRoute] from '@nuxt'
 
 export default defineEventHandler(async (event) => {
     const req = event.req
     const url = new URL(req.headers.referer)
     const params = url.searchParams
+
+    // const route = useRoute()
+    // console.log('route = ', route)
+
     try {
         const options = {
             method: 'GET',
@@ -15,6 +20,20 @@ export default defineEventHandler(async (event) => {
         const resData = humps.camelizeKeys(res.data)
         return resData
     } catch (error) {
-        console.log('error = ', error)
+        console.error('An error occurred:', error.message)
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.error('Response data:', error.response.data)
+            console.error('Response status:', error.response.status)
+            console.error('Response headers:', error.response.headers)
+        } else if (error.request) {
+            // The request was made but no response was received
+            console.error('No response received:', error.request)
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.error('Request error:', error.message)
+        }
+        console.error('Config:', error.config)
     }
 })
