914e3bb536466b7a79581e82bdaf1d56f63f6d3c
bonomite <thomasbono.com@gmail.com>
2024-01-23 15:24:57 -0500

    removed alerts, hoping it still all works perfectly without the delays

--- app.vue
+++ app.vue
@@ -141,16 +141,16 @@ const addListeners = async () => {
   const client = useSupabaseClient()
   await App.addListener("appUrlOpen", async (event: URLOpenListenerEvent) => {
     //when redirected to the app from a deep link, we need to exchange the url parame code for a session
-    alert("event = " + JSON.stringify(event))
-    console.log("event = ", event)
+    //alert("event = " + JSON.stringify(event))
+    //console.log("event = ", event)
     const code = event.url.split("=")[1]
-    console.log("code = ", code)
-    alert("code = " + JSON.stringify(code))
+    //console.log("code = ", code)
+    //alert("code = " + JSON.stringify(code))
     if (code) {
       await client.auth.exchangeCodeForSession(code)
-      alert("route")
+      //alert("route")
       navigateTo("/")
-      alert("refresh")
+      //alert("refresh")
       window.location.reload()
     }
   })
