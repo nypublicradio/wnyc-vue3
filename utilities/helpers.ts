63fc0de9b65758c2e83c7e6ea50a750d780419be
Kim LaRocca <kim@kimlarocca.com>
2024-01-30 09:08:16 -0500

    add user id to click events

--- utilities/helpers.ts
+++ utilities/helpers.ts
@@ -154,7 +154,7 @@ export const trackAudioEvent = (eventName, audioType, audioTitle, audioShow) => 
     audio_type: audioType,
     audio_title: audioTitle,
     audio_show: audioShow,
-    client_id: currentUser.value?.id
+    wnyc_user_id: currentUser.value?.id
   })
 }
 
@@ -166,7 +166,7 @@ export const trackClickEvent = (category, component, label) => {
     event_category: category,
     component: component,
     event_label: label,
-    client_id: currentUser.value?.id
+    wnyc_user_id: currentUser.value?.id
   })
 }
 
