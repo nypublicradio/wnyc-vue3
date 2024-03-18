a9fd6380ecc2bc48c8636eb7c41d67bb2a326152
bonomite <thomasbono.com@gmail.com>
2024-03-18 15:26:24 -0400

    added spot where to upsert the device id in advertising-id.js file

--- utilities/advertising-id.js
+++ utilities/advertising-id.js
@@ -1,17 +1,19 @@
 
 import {
-    useAdvertisingId,
+    useAdvertisingId, useCurrentUser
 } from "~/composables/states"
 import { AdvertisingId } from '@capacitor-community/advertising-id'
 
 // Get the advertising ID
 export const initAdvertisingId = async () => {
     const advertisingId = useAdvertisingId()
+    const currentUser = useCurrentUser()
     try {
         await AdvertisingId.requestTracking()
         const id = await AdvertisingId.getAdvertisingId()
         advertisingId.value = id.id
-        //alert("AdvertisingId in app = " + JSON.stringify(advertisingId.value))
+
+        // Save the advertising ID to the deviceId table in supabase here IF the user is logged in
     } catch (error) {
         console.error("Error getting advertising ID:", error)
     }
