f39e45b9e5cd9222a31968e58453c606bbecd612
bonomite <thomasbono.com@gmail.com>
2024-03-21 17:58:35 -0400

    search field fixed

--- pages/browse/index.vue
+++ pages/browse/index.vue
@@ -106,89 +106,90 @@ onMounted(() => {
         <!-- <i class="pi pi-spin pi-spinner" /> -->
       </span>
     </section>
-    <div v-if="!searchFieldValue">
-      <div class="topics">
-        <section>
-          <h2>Browse By Topic</h2>
-        </section>
-        <HorizontalScrollFeature class="topics-holder">
-          <div class="flex gap-3">
-            <div v-for="topic in showTopics" class="station-holder" :key="topic.label">
-              <div class="relative topic-btn-holder">
-                <Button
-                  class="topic-btn text-sm white-space-nowrap font-meta"
-                  :label="topic.label"
-                  @click="selectTopic(topic)"
-                  :style="`background-color: ${topic.color};`"
-                />
+    <div class="content-holder">
+      <div v-if="!searchFieldValue">
+        <div class="topics">
+          <section>
+            <h2>Browse By Topic</h2>
+          </section>
+          <HorizontalScrollFeature class="topics-holder">
+            <div class="flex gap-3">
+              <div v-for="topic in showTopics" class="station-holder" :key="topic.label">
+                <div class="relative topic-btn-holder">
+                  <Button
+                    class="topic-btn text-sm white-space-nowrap font-meta"
+                    :label="topic.label"
+                    @click="selectTopic(topic)"
+                    :style="`background-color: ${topic.color};`"
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        </HorizontalScrollFeature>
+          </HorizontalScrollFeature>
+        </div>
+        <FetchError v-if="error || shows === undefined" @on-click="refresh" />
+        <section class="tabs mt-2">
+          <TabView :lazy="true">
+            <TabPanel header="Featured Shows">
+              <div class="shows flex flex-column gap-5">
+                <template v-if="!pending">
+                  <ShowItem
+                    v-for="show in featuredShows"
+                    :data="show"
+                    :key="show.title"
+                    @onClick="goToShowPage(show)"
+                  />
+                </template>
+                <skeleton-show-item
+                  v-else
+                  v-for="(show, index) in 27"
+                  :key="`sk1-${index}`"
+                />
+              </div>
+            </TabPanel>
+            <TabPanel header="All Shows">
+              <div class="shows flex flex-column gap-5">
+                <template v-if="!pending">
+                  <ShowItem
+                    v-for="show in allShows"
+                    :data="show"
+                    :key="show.title"
+                    @onClick="goToShowPage(show)"
+                  />
+                </template>
+                <skeleton-show-item
+                  v-else
+                  v-for="(show, index) in 27"
+                  :key="`sk2-${index}`"
+                />
+              </div>
+            </TabPanel>
+          </TabView>
+        </section>
       </div>
-      <FetchError v-if="error || shows === undefined" @on-click="refresh" />
-      <section class="tabs mt-2">
-        <TabView :lazy="true">
-          <TabPanel header="Featured Shows">
-            <div class="shows flex flex-column gap-5">
-              <template v-if="!pending">
-                <ShowItem
-                  v-for="show in featuredShows"
-                  :data="show"
-                  :key="show.title"
-                  @onClick="goToShowPage(show)"
-                />
-              </template>
-              <skeleton-show-item
-                v-else
-                v-for="(show, index) in 27"
-                :key="`sk1-${index}`"
-              />
-            </div>
-          </TabPanel>
-          <TabPanel header="All Shows">
-            <div class="shows flex flex-column gap-5">
-              <template v-if="!pending">
-                <ShowItem
-                  v-for="show in allShows"
-                  :data="show"
-                  :key="show.title"
-                  @onClick="goToShowPage(show)"
-                />
-              </template>
-              <skeleton-show-item
-                v-else
-                v-for="(show, index) in 27"
-                :key="`sk2-${index}`"
-              />
-            </div>
-          </TabPanel>
-        </TabView>
-      </section>
-    </div>
-    <div v-else>
-      <section class="results">
-        <!-- if results show them -->
-        <div class="results-list mb-2">
-          <h2>Search Results</h2>
-        </div>
-        <div class="shows flex flex-column gap-5">
-          <ShowItem
-            v-for="show in search.results"
-            :data="show.item"
-            :key="show.item.title"
-            @onClick="goToShowPage(show.item)"
-          />
-        </div>
-        <!-- if no results show this -->
-        <div
-          v-if="search.results.length === 0"
-          class="text-center flex flex-column gap-4 mt-8"
-        >
-          <h2>No results for "{{ searchFieldValue }}"</h2>
-          <NoResultsGraphic class="max-w-6rem m-auto" alt="No Results" />
-          <div>
-            <!-- <p class="mb-2">
+      <div v-else>
+        <section class="results">
+          <!-- if results show them -->
+          <div class="results-list mb-2">
+            <h2>Search Results</h2>
+          </div>
+          <div class="shows flex flex-column gap-5">
+            <ShowItem
+              v-for="show in search.results"
+              :data="show.item"
+              :key="show.item.title"
+              @onClick="goToShowPage(show.item)"
+            />
+          </div>
+          <!-- if no results show this -->
+          <div
+            v-if="search.results.length === 0"
+            class="text-center flex flex-column gap-4 mt-8"
+          >
+            <h2>No results for "{{ searchFieldValue }}"</h2>
+            <NoResultsGraphic class="max-w-6rem m-auto" alt="No Results" />
+            <div>
+              <!-- <p class="mb-2">
               Did you mean:
               <Button
                 link
@@ -198,13 +199,14 @@ onMounted(() => {
                 @click="searchFieldValue = getAlgoliaSuggestion"
               />
             </p> -->
-            <p>
-              Try searching again using<br />
-              different keywords or spelling
-            </p>
+              <p>
+                Try searching again using<br />
+                different keywords or spelling
+              </p>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   </div>
 </template>
@@ -212,7 +214,7 @@ onMounted(() => {
 <style lang="scss" scoped>
 .browse-page {
   .search {
-    position: sticky;
+    position: fixed;
     top: env(safe-area-inset-top);
     background: var(--backgroundSimple);
     z-index: 1;
@@ -226,35 +228,38 @@ onMounted(() => {
     margin-top: -1.25rem;
     margin-left: -2.5rem;
   }
-  .topics {
-    .topic-btn-holder {
-      .topic-btn {
-        border: 1px solid transparent !important;
-        &:hover,
-        &:focus,
-        &:active {
+  .content-holder {
+    padding-top: calc(env(safe-area-inset-top) + 72px);
+    .topics {
+      .topic-btn-holder {
+        .topic-btn {
           border: 1px solid transparent !important;
+          &:hover,
+          &:focus,
+          &:active {
+            border: 1px solid transparent !important;
+          }
         }
-      }
 
-      .arrow {
-        transition: bottom 0.5s;
-        -webkit-transition: bottom 0.5s;
-        position: absolute;
-        bottom: 0px;
-        right: 0;
-        left: 0;
-        margin: auto;
-        width: 0;
-        height: 0;
-        border-left: 10px solid transparent;
-        border-right: 10px solid transparent;
-        border-top: 10px solid var(--red);
-        z-index: -1;
-      }
-      &.activetopicholder {
         .arrow {
-          bottom: -10px;
+          transition: bottom 0.5s;
+          -webkit-transition: bottom 0.5s;
+          position: absolute;
+          bottom: 0px;
+          right: 0;
+          left: 0;
+          margin: auto;
+          width: 0;
+          height: 0;
+          border-left: 10px solid transparent;
+          border-right: 10px solid transparent;
+          border-top: 10px solid var(--red);
+          z-index: -1;
+        }
+        &.activetopicholder {
+          .arrow {
+            bottom: -10px;
+          }
         }
       }
     }
