bb3d4183e5c7f39c78552019c867d3d6ceee92e7
bonomite <thomasbono.com@gmail.com>
2024-03-29 19:12:30 -0400

    I am no longer canceling the notifications...

--- utilities/local-notifications.ts
+++ utilities/local-notifications.ts
@@ -73,16 +73,16 @@ export const scheduleLocalNotification = async (entry) => {
 
 // loops through all pending notifications and cancels them
 export const cancelAllNotifications = async () => {
-    try {
-        // Get all pending notifications
-        const pendingNotifications = await LocalNotifications.getPending();
+    // try {
+    //     // Get all pending notifications
+    //     const pendingNotifications = await LocalNotifications.getPending();
 
-        // Cancel all pending notifications
-        await LocalNotifications.cancel({ notifications: pendingNotifications.notifications });
-        setPendingLocalNotifications()
-    } catch (error) {
-        console.error('Error canceling all notifications:', error);
-    }
+    //     // Cancel all pending notifications
+    //     await LocalNotifications.cancel({ notifications: pendingNotifications.notifications });
+    //     setPendingLocalNotifications()
+    // } catch (error) {
+    //     console.error('Error canceling all notifications:', error);
+    // }
 };
 
 // initialize local notifications and add listener to handle when tapping on a notification and what the app should do
