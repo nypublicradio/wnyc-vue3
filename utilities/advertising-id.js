13905e9b867ac59d379cc925e2c7e060334d740a
Kim LaRocca <kim@kimlarocca.com>
2024-04-18 11:17:51 -0400

    swap advertising id with device id

--- utilities/advertising-id.js
+++ utilities/advertising-id.js
@@ -3,28 +3,27 @@ import {
     useCurrentUser
 } from "~/composables/states"
 import {
-    AdvertisingId
-} from '@capacitor-community/advertising-id'
+    Device
+} from '@capacitor/device';
 
 // Get the advertising ID
 export const initAdvertisingId = async () => {
     const advertisingId = useAdvertisingId()
     const currentUser = useCurrentUser()
     try {
-        await AdvertisingId.requestTracking()
-        const id = await AdvertisingId.getAdvertisingId()
-        advertisingId.value = id.id
-        if (currentUser.value) {
+        const id = await Device.getId()
+        advertisingId.value = id
+        if ( currentUser.value ) {
             // save the advertising ID to the deviceId table in supabase IF the user is logged in
             const client = useSupabaseClient()
             await client
-                .from('device_ids')
-                .upsert([{
+                .from( 'device_ids' )
+                .upsert( [ {
                     user_id: currentUser.value.id,
                     device_id: advertisingId.value
-                },])
+                }, ] )
         }
-    } catch (error) {
-        console.error("Error getting advertising ID:", error)
+    } catch ( error ) {
+        console.error( "Error getting advertising ID:", error )
     }
 }
