6e871617599fa52a2fddd2b327fe3b9230f7f4d5
bonomite <thomasbono.com@gmail.com>
2024-04-19 18:47:01 -0400

    added error page, need to show error from fetches if they fail

---
+++ error.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps({
+  error: Object,
+})
+
+function handleGoHome() {
+  clearError({ redirect: "/home" })
+}
+function handleTryAgain() {
+  clearError()
+}
+</script>
+
+<template>
+  <div class="">
+    <div class="flex flex-column gap-3 py-4 px-5">
+      <h1>{{ error?.message }}</h1>
+      There was an error 😱
+
+      <br />
+      <div class="mt-3 flex gap-3 justify-content-center">
+        <Button
+          label="Try again"
+          severity="secondary"
+          aria-label="try again"
+          @click="handleTryAgain"
+        />
+        <Button label="Navigate home" aria-label="Navigate home" @click="handleGoHome" />
+      </div>
+    </div>
+  </div>
+</template>
--- plugins/error-handler.js
+++ plugins/error-handler.js
@@ -1,6 +1,20 @@
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.hook('vue:error', (error) => {
-
-        window.location.replace('/home')
+    nuxtApp.hook('vue:error', (..._args) => {
+        console.log('vue:error = ', _args)
+        // if (process.client) {
+        //   console.log(..._args)
+        // }
     })
+    nuxtApp.hook('app:error', (..._args) => {
+        console.log('app:error = ', _args)
+        // if (process.client) {
+        //   console.log(..._args)
+        // }
+    })
+    nuxtApp.vueApp.config.errorHandler = (..._args) => {
+        console.log('global error handler = ', _args)
+        // if (process.client) {
+        //   console.log(..._args)
+        // }
+    }
 })
