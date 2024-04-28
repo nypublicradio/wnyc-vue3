54dfd57e569885e75b2e91c3cf2ba2bb93d216f9
bonomite <thomasbono.com@gmail.com>
2024-04-28 13:15:25 -0400

    gif handle in HtmlConvert and img size optimize

--- components/HtmlConvert.vue
+++ components/HtmlConvert.vue
@@ -8,6 +8,17 @@ const props = defineProps({
     default: "",
   },
 })
+// check if the string is HTML
+const isHTML = (str) => {
+  const doc = new DOMParser().parseFromString(str, "text/html")
+  return Array.from(doc.body.childNodes).some((node) => node.nodeType === 1)
+}
+const isGif = (imageUrl) => {
+  const extension = imageUrl.split(".").pop()?.toLowerCase()
+  return extension === "gif"
+}
+
+const imgWidth = ref(null)
 
 const parseHtml = computed(() => {
   const updatedHTML = props.htmlContent
@@ -18,17 +29,18 @@ const parseHtml = computed(() => {
         : `<a href="${href}" target="_blank" rel="noopener noreferrer">${text}</a>`
     })
     .replace(/<img[^>]*src="([^"]+)"[^>]*alt="([^"]+)"[^>]*>/g, (match, src, alt) => {
-      return `<VImage src="${src}" alt="${alt}" :width="${400}" />`
+      //return `<VImage src="${src}" alt="${alt}" :width="${400}" />`
+      return isGif(src)
+        ? `<img src="${src}" alt="${alt}" />`
+        : `<VImage src="${src}" alt="${alt}" :width="${imgWidth.value}" />`
     })
 
   return updatedHTML
 })
 
-// check if the string is HTML
-const isHTML = (str) => {
-  const doc = new DOMParser().parseFromString(str, "text/html")
-  return Array.from(doc.body.childNodes).some((node) => node.nodeType === 1)
-}
+onMounted(() => {
+  window.innerWidth > 768 ? (imgWidth.value = 672) : (imgWidth.value = 382)
+})
 </script>
 
 <template>
