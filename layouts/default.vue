f8933a8e4fbc862b01197a8be6b788f433e38c55
Digital <digital@DIG-MBP-10563.local>
2024-05-01 10:22:59 -0400

    fixed home header top safe area animation issue

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,8 +30,8 @@ useHead({
 <style lang="scss">
 header {
   .v-smart-header {
-    margin-top: -$headerHeight !important;
-    transition: all $transitionDuration ease;
+    margin-top: calc(var(--header-height) * -1 + env(safe-area-inset-top)) !important;
+    transition: margin-top $transitionDuration ease;
   }
 }
 header.show {
