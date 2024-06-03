686178b75b1cafdd3fbe5e3d9c0e8510ac3ddcea
bonomite <thomasbono.com@gmail.com>
2024-06-03 15:34:51 -0400

    attempt to update the webview and not have the font size reflext the systems

--- android/app/src/main/java/org/wnyc/android/MainActivity.java
+++ android/app/src/main/java/org/wnyc/android/MainActivity.java
@@ -1,5 +1,17 @@
 package org.wnyc.android;
-
+import android.content.res.Configuration;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+   @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.custom_web_view);
+        WebSettings settings = webView.getSettings();
+        settings.setTextZoom(100);
+    }
+}
