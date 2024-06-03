3bb5e6ea555bd5bed42fdf196cc9c3fe005d00e1
bonomite <thomasbono.com@gmail.com>
2024-06-03 16:09:19 -0400

    fixed

--- android/app/src/main/java/org/wnyc/android/MainActivity.java
+++ android/app/src/main/java/org/wnyc/android/MainActivity.java
@@ -1,16 +1,17 @@
 package org.wnyc.android;
-import android.content.res.Configuration;
+
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
 
-   @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        WebView webView = findViewById(R.id.custom_web_view);
+        WebView webView = getBridge().getWebView();
         WebSettings settings = webView.getSettings();
         settings.setTextZoom(100);
     }
