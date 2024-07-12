a46db8d17194f893c58e65544ff457d1a86bddca
Josh <jgitlin@nypublicradio.org>
2024-07-12 13:27:07 -0400

    remove broken copilot code

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -27,31 +27,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    override func becomeFirstResponder() -> Bool {
-        return true
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        UIApplication.shared.beginReceivingRemoteControlEvents()
-        self.becomeFirstResponder()
-    }
-
-    override func remoteControlReceived(with event: UIEvent?) {
-        guard let event = event else { return }
-        if event.type == .remoteControl {
-            switch event.subtype {
-            case .remoteControlPlay:
-                player.play()
-            case .remoteControlPause:
-                player.pause()
-            // Handle other cases like .remoteControlNextTrack, .remoteControlPreviousTrack
-            default:
-                break
-            }
-        }
-    }
-    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
