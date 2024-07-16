3243a1c7401fc92f697c9fd1fc96f04a5b98319b
bonomite <thomasbono.com@gmail.com>
2024-07-16 16:45:48 -0400

    updated to capacitor v6

--- components/AudioPlayer.vue
+++ components/AudioPlayer.vue
@@ -34,7 +34,7 @@ import {
   getEpisodeFallBackImage,
 } from "~/utilities/helpers"
 
-import { initMediaSession } from "~/utilities/media-session.js"
+//import { initMediaSession } from "~/utilities/media-session.js"
 import "vidstack/bundle"
 //import { MediaRemoteControl } from "vidstack"
 
@@ -92,7 +92,7 @@ const updateUseIsEpisodePlaying = async (e) => {
       playerRef.value.togglePlay()
       await nextTick()
       // initiallizes the media session in ~/utilities/media-session.js
-      initMediaSession(currentEpisode.value, skipTime)
+      //initMediaSession(currentEpisode.value, skipTime)
     }, 1500)
   }
 }
@@ -118,7 +118,7 @@ const switchEpisode = () => {
   //separagte delay for the media session to init
   setTimeout(() => {
     // initiallizes the media session in ~/utilities/media-session.js
-    initMediaSession(currentEpisode.value, skipTime)
+    //initMediaSession(currentEpisode.value, skipTime)
   }, 1000)
 }
 
@@ -248,7 +248,7 @@ const handleIsExpanded = (e) => {
   // it hs to be on a delay, because then the media session artwork image does not show up sometimes
 
   setTimeout(() => {
-    initMediaSession(currentEpisode.value, skipTime)
+    //(currentEpisode.value, skipTime)
   }, 1500)
 }
 
