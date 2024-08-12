e2048309a0051ca5332c876d2a76943c2b56d75c
Thomas Bono <thomasbono.com@gmail.com>
2024-08-12 14:12:05 -0400

    added API_KEY for all 3 platforms

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -7,21 +7,25 @@ export default defineNuxtPlugin(async () => {
 
     const platform = await Capacitor.getPlatform()
     let thisAppId = null
+    let thisApiKey = null
     switch (platform) {
         case 'web':
             thisAppId = config.public.FB_APP_ID_WEB
+            thisApiKey = config.public.FB_API_KEY_WEB
             break
         case 'android':
             thisAppId = config.public.FB_APP_ID_ANDROID
+            thisApiKey = config.public.FB_API_KEY_ANDROID
             break
         case 'ios':
             thisAppId = config.public.FB_APP_ID_IOS
+            thisApiKey = config.public.FB_API_KEY_IOS
             break
         default:
     }
 
     const firebaseConfig = {
-        apiKey: config.public.FB_API_KEY,
+        apiKey: thisApiKey,
         authDomain: config.public.FB_AUTH_DOMAIN,
         projectId: config.public.FB_PROJECT_ID,
         storageBucket: config.public.FB_STORAGE_BUCKET,
