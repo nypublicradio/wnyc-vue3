fc63e0d6e8be08a8fb6b98f275939c2c1a428cf8
Thomas Bono <thomasbono.com@gmail.com>
2024-08-17 13:21:12 -0400

    ios update for js background

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -14,6 +14,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    private func getWebView() -> WKWebView? {
+        return (window?.rootViewController as? CAPBridgeViewController)?.bridge?.webView
+    }
+    func willFinishLoadingWithOptions(_ application: UIApplication) {
+        guard let webView = getWebView() else {
+            print("Unable to setup WebView: WebView is nil")
+            return
+        }
+        
+        if #available(iOS 17.0, *) {
+            webView.configuration.preferences.inactiveSchedulingPolicy = .none
+        }
+
+    }
+
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
