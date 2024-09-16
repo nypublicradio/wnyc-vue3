a8b9eba43af026c5cca865a055cb2ce377a960a6
Kim LaRocca <kim@kimlarocca.com>
2024-09-16 11:40:17 -0400

    take params out of the array brackets; add fallback for userid

--- plugins/analytics.client.ts
+++ plugins/analytics.client.ts
@@ -7,17 +7,17 @@ export default defineNuxtPlugin(() => {
   const sendEvent = async (name, params) => {
     await FirebaseAnalytics.logEvent({
       name,
-      params: [params],
+      params: params,
     });
   }
   // gtag event for reporting on page views
   const sendPageView = (params) => {
     const currentUser = useCurrentUser()
+    const deviceId = useDeviceId()
     sendEvent('page_view', {
       page_location: document.location.href,
       page_title: document.title,
-      user_id: currentUser.value?.id,
-      //NYPRMember: membershipStatus.value,
+      user_id: currentUser.value?.id ?? deviceId.value,
       ...params
     })
   }
