f36057c805d9d34ad3613a4c427ba0e9ace1cf57
Thomas Bono <thomasbono.com@gmail.com>
2025-02-24 13:51:01 -0500

    deep source

--- assets/copyToAssets.js
+++ assets/copyToAssets.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-fs.copyFile(`./public/splash.png`, `./assets/splash.png`, (err) => {
+fs.copyFile('./public/splash.png', './assets/splash.png', (err) => {
     if (err) throw err
 })
 require('dotenv').config()
 const env = process.env.ENV
-fs.copyFile(`./public/icon-only-${env}.png`, `./assets/icon-only.png`, (err) => {
+fs.copyFile(`./public/icon-only-${env}.png`, './assets/icon-only.png', (err) => {
     if (err) throw err
 })
