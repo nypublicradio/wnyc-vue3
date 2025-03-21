156553fe48c7cfbb7fb3641bafcb3b097e9d19a1
Thomas Bono <thomasbono.com@gmail.com>
2025-03-21 18:32:24 -0400

    merged menu template with wagtail primary navigation, fixed rollover to mouseenter

--- composables/useNavigationData.ts
+++ composables/useNavigationData.ts
@@ -1,7 +1,9 @@
+const config = useRuntimeConfig()
+
 const headerMenuTemplate = [
     {
         label: "Live Radio",
-        url: "/live",
+        //url: "/live",
         id: "0",
         icon: "",
         hasSubmenu: true,
@@ -13,7 +15,7 @@ const headerMenuTemplate = [
                 },
                 {
                     label: "Daily Schedule",
-                    url: "/schedule"
+                    url: "/live"
                 },
             ],
         ],
@@ -57,13 +59,49 @@ const headerMenuTemplate = [
 
 ]
 
-export default function useDarkMode() {
+const normalizeWagtailMenuData = (menuData) => {
+    return menuData.map((item) => ({
+        'label': item.value.title,
+        'url': item.value.url,
+        'icon': '',
+        'id': String(item.id),
+        'type': item.type,
+        'hasSubmenu': false,
+    }))
+}
 
-    const headerMenuData = useState('headerMenuData', () => headerMenuTemplate)
+export default async function useNavigationData() {
+    // Fetch data concurrently using Promise.all
+    const [wagtailNavigationResponse, donateResponse] = await Promise.all([
+        useFetch(config.public.HEADER_NAVIGATION_API),
+        useFetch(config.public.SYSTEM_MESSAGES_API)
+    ]);
 
-    const setDarkMode = () => {
+    const wagtailNavigationData = ref(wagtailNavigationResponse.data.value);
+    const headerNavigationData = useState('headerNavigationDataM', () => null);
+    const donateButtonData = useState('donateButtonData', () => ({
+        buttonText: '',
+        buttonLink: ''
+    }));
 
+    // Process navigation data and normalize it
+    const primaryNavigation = normalizeWagtailMenuData(wagtailNavigationData.value.primary_navigation);
+
+    // merge wagtailNavigationData with headerMenuTemplate at the 2 index
+    headerNavigationData.value = headerMenuTemplate;
+    headerNavigationData.value.splice(2, 0, ...primaryNavigation);
+
+
+    // Process donate data
+    if (donateResponse.data.value?.product_banners?.length > 0) {
+        donateResponse.data.value.product_banners.forEach((banner) => {
+            if (banner.value.title === "WNYC App Donate Button") {
+                donateButtonData.value.buttonText = banner.value.button_text;
+                donateButtonData.value.buttonLink = banner.value.button_link;
+            }
+        });
     }
 
-    return { setDarkMode, headerMenuData }
+    return { headerNavigationData, donateButtonData };
 }
+
