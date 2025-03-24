b6aac5b5fb5a28d83b89fa6e8a1cb4dbb1f62f33
Thomas Bono <thomasbono.com@gmail.com>
2025-03-24 17:05:41 -0400

    started the browser desktop menu

---
+++ components/SettingsBrowser.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { onMounted } from "vue"
+import {
+  trackClickEvent,
+  getYear,
+  setFontSize,
+  setDarkMode,
+  toggleAskNotificationPermissions,
+  //toSystemSettings,
+} from "~/utilities/helpers"
+import {
+  useAllCurrentStations,
+  useTextSizeOption,
+  useCurrentUser,
+  useCurrentUserProfile,
+  useEditProfileSideBar,
+  useIsLiveStream,
+  useIsApp,
+  useAccountDeleteSideBar,
+  useGlobalToast,
+} from "~/composables/states.ts"
+import { Preferences } from "@capacitor/preferences"
+import { localUserProfileKey } from "~/composables/globals"
+import { updateLiveStream } from "~/composables/data/liveStream"
+const globalToast = useGlobalToast()
+const config = useRuntimeConfig()
+const currentUser = useCurrentUser()
+const currentUserProfile = useCurrentUserProfile()
+const textSizeOptions = useTextSizeOption()
+const editProfileSideBar = useEditProfileSideBar()
+const isLiveStream = useIsLiveStream()
+const isApp = useIsApp()
+const accountDeleteSideBar = useAccountDeleteSideBar()
+
+const allCurrentStations = useAllCurrentStations()
+const stationsMenuData = ref([])
+const client = useSupabaseClient()
+</script>
+
+<template>
+  <div class="settings-desktop style-mode-dark">
+    <section class="footer mb-4">
+      <WnycLogo style="fill: var(--bw-toggle)" />
+      <p>© {{ getYear() }} New York Public Radio. All rights reserved.</p>
+      <p>Version {{ config.public.APP_VERSION }}</p>
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.settings-desktop {
+  background-color: var(--p-surface-950);
+  height: 100%;
+  section {
+    margin-bottom: 30px;
+  }
+  .footer {
+    text-align: center;
+    .wnyc-logo {
+      width: 60px;
+      height: auto;
+      margin-bottom: 10px;
+      fill: var(--p-surface-950);
+    }
+  }
+}
+</style>
