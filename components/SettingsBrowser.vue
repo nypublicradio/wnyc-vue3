95fd462cc7b13567878794adf355eb4ba3b263a5
Thomas Bono <thomasbono.com@gmail.com>
2025-03-25 17:00:15 -0400

    almosrt done with the hamburger

--- components/SettingsBrowser.vue
+++ components/SettingsBrowser.vue
@@ -1,4 +1,5 @@
 <script setup>
+import useNavigationData from "~/composables/useNavigationData"
 import { onMounted } from "vue"
 import {
   trackClickEvent,
@@ -18,6 +19,7 @@ import {
   useIsApp,
   useAccountDeleteSideBar,
   useGlobalToast,
+  useSettingsSideBarBrowser,
 } from "~/composables/states.ts"
 import { Preferences } from "@capacitor/preferences"
 import { localUserProfileKey } from "~/composables/globals"
@@ -35,10 +37,57 @@ const accountDeleteSideBar = useAccountDeleteSideBar()
 const allCurrentStations = useAllCurrentStations()
 const stationsMenuData = ref([])
 const client = useSupabaseClient()
+
+const settingsSideBarBrowser = useSettingsSideBarBrowser()
+const { allNavigationData } = await useNavigationData()
 </script>
 
 <template>
-  <div class="settings-desktop style-mode-dark">
+  <div
+    class="settings-desktop style-mode-dark"
+    :class="[{ 'logged-in': currentUser, 'logged-out': !currentUser }]"
+  >
+    <div class="menu px-4 py-6 flex">
+      <!-- <p class="text-xs"><pre>{{ allNavigationData }}</pre></p> -->
+      <div
+        v-for="item in allNavigationData"
+        :key="item.id"
+        class="menu-holder"
+        :class="item.class"
+      >
+        <h1 class="menu-h1">{{ item.label }}</h1>
+        <div class="flex flex-column gap-2 -ml-2">
+          <VFlexibleLink
+            v-for="item in item.items[0]"
+            :key="item.id"
+            raw
+            :to="item.url"
+            class="menu-btn"
+            :class="item.class"
+            @flexible-link-click="
+              () => {
+                settingsSideBarBrowser = false
+                item.command()
+                trackClickEvent(
+                  `Click Tracking - ${item.label} Button`,
+                  'hamburger menu',
+                  `${item.label} Button`
+                )
+              }
+            "
+          >
+            <Button
+              raw
+              :label="item.label"
+              :aria-label="`${item.label} button`"
+              severity="secondary"
+              size="small"
+              variant="link"
+            />
+          </VFlexibleLink>
+        </div>
+      </div>
+    </div>
     <section class="footer mb-4">
       <WnycLogo style="fill: var(--bw-toggle)" />
       <p>© {{ getYear() }} New York Public Radio. All rights reserved.</p>
@@ -54,6 +103,24 @@ const client = useSupabaseClient()
   section {
     margin-bottom: 30px;
   }
+  .menu {
+    gap: 4rem;
+    flex-wrap: wrap;
+    .menu-h1 {
+      margin-bottom: 16px;
+      @include media("<lg") {
+        font-size: 0.813rem;
+        text-transform: uppercase;
+        font-weight: var(--font-weight-400);
+        margin-bottom: 8px;
+      }
+    }
+    .menu-holder {
+      min-width: 25vh;
+      .menu-btn {
+      }
+    }
+  }
   .footer {
     text-align: center;
     .wnyc-logo {
@@ -61,6 +128,40 @@ const client = useSupabaseClient()
       height: auto;
       margin-bottom: 10px;
       fill: var(--p-surface-950);
+    }
+  }
+  &.logged-out {
+    .menu-holder {
+      &.saved {
+        display: none;
+      }
+      &.account {
+        .logout {
+          display: none;
+        }
+      }
+    }
+  }
+  &.logged-in {
+    .menu-holder {
+      &.account {
+        .login,
+        .signup {
+          display: none;
+        }
+      }
+    }
+  }
+}
+</style>
+
+<style lang="scss">
+.settings-desktop {
+  .menu-holder {
+    .menu-btn {
+      .p-button-label {
+        text-align: left;
+      }
     }
   }
 }
