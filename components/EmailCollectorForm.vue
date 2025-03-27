d9355ad6d7c79aa6df40874f06c31160501f8edb
Thomas Bono <thomasbono.com@gmail.com>
2025-03-27 14:07:40 -0400

    newsletter added from gothamist, more footer layout

---
+++ components/EmailCollectorForm.vue
@@ -0,0 +1,231 @@
+<script setup lang="ts">
+import { watch } from 'vue'
+
+const props = defineProps({
+  showNoThanks: {
+    type: Boolean,
+    default: false,
+  },
+  dark: {
+    type: Boolean,
+    default: false,
+  },
+  outlined: {
+    type: Boolean,
+    default: false,
+  },
+  isSubmitting: {
+    type: Boolean,
+    default: false,
+  },
+  altDesign: {
+    type: Boolean,
+    default: false,
+  },
+  submissionStatus: {
+    type: String,
+    default: null,
+  },
+  submitButtonText: {
+    type: String,
+    default: 'Sign up',
+  },
+  submitButtonIcon: {
+    type: String,
+    default: null,
+  },
+  thanksMessage: {
+    type: String,
+    default: 'Thank you for signing up!',
+  },
+})
+
+const emit = defineEmits<{
+  (e: 'submit', value: any): void
+  (e: 'noThanksClick'): void
+}>()
+
+const email = ref('')
+const checked = ref(true)
+const submitButtonRef = ref(null)
+const submitButtonWidth = ref(40)
+const emailErrorText = ref(null)
+onMounted(() => {
+  // for some reason the submit button is not always rendered when the component is mounted coming from a route transition, so I have to add a slight delay to make sure it gets access to the button.
+  // this is a known bug: https://github.com/nuxt/framework/issues/3587
+  setTimeout(() => {
+    // gets the width of the submit button to set the padding right on the input field
+    if (submitButtonRef.value)
+      submitButtonWidth.value = submitButtonRef.value.offsetWidth + 20
+  }, 500)
+})
+
+// function to check if the email is a valid format and to set the error text
+function validateEmail() {
+  // See also isValidEmail in useNewsletterSignup.ts
+  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email#basic_validation
+  const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+  emailErrorText.value = null
+  if (validRegex.test(email.value)) {
+    return true
+  }
+  else {
+    emailErrorText.value = 'Please enter a valid email address.'
+    return false
+  }
+}
+
+// watches for changes in the submissionStatus prop and sets error messages accordingly
+watch(
+  () => props.submissionStatus,
+  () => {
+    emailErrorText.value = null
+    switch (props.submissionStatus) {
+      case 'success':
+        break
+      case 'error':
+        emailErrorText.value
+          = 'Sorry, there was an error with your submission. Please try again!'
+        break
+      default:
+    }
+  },
+)
+
+// submit the email value through the emit if the email is valid
+function submitForm() {
+  if (validateEmail())
+    emit('submit', email.value)
+}
+</script>
+
+<template>
+  <div>
+    <div class="email-collector-form">
+      <!-- when the submissionStatus is 'success' it will hide the form and show Thank you message -->
+      <span v-if="submissionStatus !== 'success'" class="flex flex-column lg:flex-row" style="gap: 1rem">
+        <div class="flex-grow-1">
+          <p class="mb-2" :class="emailErrorText ? 'p-error' : ''" for="email">Email address</p>
+          <div class="flex align-items-center w-full gap-2">
+            <!-- <i ref="submitButtonRef" class="submit-icon" :class="[
+              { altDesignIcon: altDesign && submitButtonIcon },
+            ]" :data-style-mode="dark ? 'dark' : 'default'">
+              <Button :disabled="isSubmitting || !checked" class="submit-btn p-button-rounded"
+                :class="[{ 'p-button-outlined': outlined }]" :icon="submitButtonIcon ? `pi ${submitButtonIcon}` : null"
+                icon-pos="right" :label="submitButtonIcon ? null : submitButtonText" :aria-label="submitButtonText"
+                @click="submitForm">
+                <i v-if="isSubmitting" class="pi pi-spin pi-spinner" />
+              </Button>
+            </i> -->
+            <div class="field m-0 flex flex-grow-1">
+              <InputText v-model="email" :disabled="isSubmitting" class="p-inputtext-lg w-full" :class="[
+                { 'p-invalid': emailErrorText },
+                { 'alt-design': altDesign },
+                { dark },
+              ]" :style="`padding-right: ${submitButtonWidth}px`" type="email" placeholder="your@email.com"
+                aria-label="sign up" aria-describedby="email-address-field" autocomplete="email" name="email"
+                @keypress.enter="submitForm" />
+
+            </div>
+            <i ref="submitButtonRef" class="submit-icon flex-none" :class="[
+              { altDesignIcon: altDesign && submitButtonIcon },
+            ]" :data-style-mode="dark ? 'dark' : 'default'">
+              <Button :disabled="isSubmitting || !checked" class="submit-btn p-button-rounded"
+                :class="[{ 'p-button-outlined': outlined }]" :icon="submitButtonIcon ? `pi ${submitButtonIcon}` : null"
+                icon-pos="right" :label="submitButtonIcon ? null : submitButtonText" :aria-label="submitButtonText"
+                severity="secondary" @click="submitForm">
+                <i v-if="isSubmitting" class="pi pi-spin pi-spinner" />
+              </Button>
+            </i>
+          </div>
+          <Transition name="fade">
+            <small v-if="emailErrorText" id="email-address-field" class="p-error mt-1 block">{{ emailErrorText
+              }}</small>
+          </Transition>
+          <div class="field-checkbox mt-3 mb-0">
+            <Checkbox v-model="checked" aria-label="Toggle agreement to the terms" :disabled="isSubmitting"
+              :binary="true" @click="!checked" />
+            <label for="binary">
+              <slot />
+            </label>
+          </div>
+        </div>
+        <div v-if="showNoThanks" class="flex justify-content-start">
+          <div>
+            <Button class="no-thanks-btn p-button-link" label="No thanks"
+              :style="isSubmitting ? 'visibility: hidden' : ''" @click="emit('noThanksClick')" />
+          </div>
+        </div>
+      </span>
+      <p v-else class="type-paragraph3">
+        {{ thanksMessage }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.email-collector-form .p-input-icon-right {
+  .submit-icon {
+    .p-button {
+      min-height: 41px;
+      min-width: 41px;
+      padding: 0px 16px;
+    }
+
+    &.altDesignIcon {
+      margin-top: 0;
+
+      .p-button {
+        padding: 0;
+        min-height: unset;
+        min-width: unset;
+        border-radius: 20px;
+        height: 1.4rem;
+        width: 2rem;
+
+        .pi {
+          font-size: 0.75rem;
+        }
+      }
+    }
+  }
+
+  .p-inputtext {
+    padding-right: 100px;
+  }
+}
+
+.email-collector-form .pi-spinner {
+  font-size: 1.25rem;
+  color: var(--black);
+  margin: auto;
+}
+
+.email-collector-form .no-thanks-btn {
+  margin-left: -1rem;
+  width: 110px;
+  margin-top: 8px;
+  text-decoration: underline;
+
+  @include media('<md') {
+    margin-top: -1rem;
+  }
+}
+
+.email-collector-form {
+  .field-checkbox {
+    .p-checkbox {
+      align-self: flex-start;
+    }
+
+    label {
+      @include font-config($type-fineprint);
+    }
+  }
+
+  .p-error {
+    text-align: right;
+  }
+}
+</style>
--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,22 +1,49 @@
 <script lang="ts" setup>
 import { allSocialData } from "~/composables/menuData"
 import { trackClickEvent } from "~/utilities/helpers"
+import NewsletterForm from "./NewsletterForm.vue"
 const { footerNavigationData } = await useNavigationData()
 </script>
 
 <template>
   <section class="the-footer style-mode-dark py-5">
-    <div class="grid">
+    <div class="grid content">
       <div class="col-12 xl:col">
-        <div class="flex gap-5 align-items-center">
-          <div class="w-5rem flex-none">
-            <WnycLogo class="wnyc-logo cursor-pointer" />
+        <div class="flex gap-5 flex-column">
+          <div class="flex gap-5 align-items-center">
+            <div class="w-5rem flex-none">
+              <WnycLogo class="wnyc-logo cursor-pointer" />
+            </div>
+            <div>
+              <p class="line-height-3 text-xs">
+                Listener-supported WNYC is the home for independent journalism and courageous
+                conversation on air and online. Broadcasting live from New York City on 93.9
+                FM and AM 820 and available online and on the go.
+              </p>
+            </div>
           </div>
-          <p class="line-height-3 text-xs">
-            Listener-supported WNYC is the home for independent journalism and courageous
-            conversation on air and online. Broadcasting live from New York City on 93.9
-            FM and AM 820 and available online and on the go.
-          </p>
+          <div class="flex gap-5 align-items-center">
+            <div class="w-5rem flex-none hidden xl:block">
+              <WnycLogo class="hidden" />
+            </div>
+            <div>
+              <h2 class="mb-3">
+                Sign up for our newsletter
+              </h2>
+              <p class="line-height-3 text-xs">
+                Sign up for for a weekly, behind-the-scenes update from the people behind your favorite shows.
+                <VFlexibleLink to="/newsletter">See More
+                </VFlexibleLink>
+              </p>
+              <email-collector-form class="mt-3" @submit="submitForm">
+                By submitting your information, you're agreeing to receive
+                communications from New York Public Radio in accordance with our
+                <VFlexibleLink to="https://www.wnyc.org/terms/">
+                  Terms
+                </VFlexibleLink>.
+              </email-collector-form>
+            </div>
+          </div>
         </div>
       </div>
       <div class="hidden xl:block col-1"></div>
@@ -35,7 +62,7 @@ const { footerNavigationData } = await useNavigationData()
             <Button :icon="item.icon" severity="secondary" size="large" rounded />
           </VFlexibleLink>
         </div>
-        <div class="menu pt-6 flex gap-4">
+        <div class="menu pt-6 flex flex-wrap md:flex-nowrap gap-4">
           <ExpandedMenuItem v-for="item in footerNavigationData" :key="item.id" :item="item"
             :class="`menu-holder ${item.class}`" :menuData="item" />
         </div>
@@ -58,7 +85,15 @@ const { footerNavigationData } = await useNavigationData()
 
   .menu {
     .menu-holder {
-      min-width: 200px;
+      min-width: 170px;
+
+      @include media(">xxl") {
+        min-width: 200px;
+      }
+
+      @include media("<sm") {
+        min-width: 150px;
+      }
     }
   }
 }
