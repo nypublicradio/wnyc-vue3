945948d74a665bcf3bf5a7257dbf01fa2e96bd02
Thomas Bono <thomasbono.com@gmail.com>
2025-04-02 10:52:15 -0400

    bff clean up

--- server/api/navigation.ts
+++ server/api/navigation.ts
@@ -4,44 +4,36 @@ const config = useRuntimeConfig();
 
 // BFF for fetching the data only for the navigation
 async function useNavigationData() {
-    let wagtailResponse = null;
-    let donateResponse = null;
-    let stationsResponse = null;
-    let showsResponse = null;
-
     try {
         // Fetch all data concurrently
-        const [wagtailResponseGet, donateResponseGet, stationsResponseGet, showsResponseGet] = await Promise.all([
+        const [wagtail, donate, stations, shows] = await Promise.all([
             axios.get(config.public.HEADER_NAVIGATION_API),
             axios.get(config.public.SYSTEM_MESSAGES_API),
             axios.get(`${config.public.BFF_URL}/api/streams`),
             axios.get(`${config.public.BFF_URL}/api/v2/shows`),
         ]);
 
-        wagtailResponse = wagtailResponseGet.data;
-        donateResponse = donateResponseGet.data;
-        stationsResponse = stationsResponseGet.data;
-        showsResponse = showsResponseGet.data;
-
+        return {
+            wagtailResponse: wagtail.data,
+            donateResponse: donate.data,
+            stationsResponse: stations.data,
+            showsResponse: shows.data,
+        };
     } catch (fetchError) {
         console.error("Failed to fetch or process navigation data:", fetchError);
+        return {
+            wagtailResponse: null,
+            donateResponse: null,
+            stationsResponse: null,
+            showsResponse: null,
+        };
     }
-
-    return {
-        wagtailResponse,
-        donateResponse,
-        stationsResponse,
-        showsResponse,
-    };
 }
 
 export default defineEventHandler(async (event) => {
-    //console.log('getting home page TOP STORIES data')
     const res = event?.node?.res;
     res.setHeader('Cache-Control', 'maxage=120, stale-while-revalidate');
     const data = await useNavigationData();
 
-    return {
-        data,
-    }
+    return { data };
 })
