08f7a423e34095af041176946c517e14add743c4
bonomite <thomasbono.com@gmail.com>
2025-04-10 11:14:01 -0400

    added login and signup routes pages and updated the buttons with isRoute conditional

---
+++ pages/login.vue
@@ -0,0 +1,21 @@
+<script setup>
+onMounted(() => {
+  // send GA page view
+  const { $analytics } = useNuxtApp()
+  $analytics.sendPageView({
+    page_title: "Login",
+    page_type: "login_page",
+    content_group: "login",
+  })
+})
+</script>
+
+<template>
+  <Login isRoute />
+</template>
+
+<style lang="scss">
+.page.login {
+  background: var(--p-surface-25);
+}
+</style>
---
+++ pages/signup.vue
@@ -0,0 +1,21 @@
+<script setup>
+onMounted(() => {
+  // send GA page view
+  const { $analytics } = useNuxtApp()
+  $analytics.sendPageView({
+    page_title: "Sign up",
+    page_type: "signup_page",
+    content_group: "signup",
+  })
+})
+</script>
+
+<template>
+  <Signup isRoute />
+</template>
+
+<style lang="scss">
+.page.signup {
+  background: var(--p-surface-25);
+}
+</style>
