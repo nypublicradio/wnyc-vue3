b3e5ef5501b1ac29a0d43ffe47896d3ce20fdcea
bonomite <thomasbono.com@gmail.com>
2025-04-16 18:49:43 -0400

    menu closes when seecting an item

--- components/TheHeaderMenu.vue
+++ components/TheHeaderMenu.vue
@@ -32,7 +32,7 @@ const emit = defineEmits(["emit-click"])
         fontWeight="600"
       >
         <template #menu v-if="item.items">
-          <NavSubMenu :model="item?.items[0]" @emit-click="emit('emit-click')" />
+          <NavSubMenu :model="item?.items[0]" @emit-click="emit('emit-click', event)" />
         </template>
       </NavButton>
     </section>
