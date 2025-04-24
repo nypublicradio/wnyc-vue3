ce44e18dd2ea32c79b8d6e40b8874485ea5ef270
Thomas Bono <thomasbono.com@gmail.com>
2025-04-24 18:20:28 -0400

    converted to a timeline and put it into its own component

---
+++ components/icons/HamburgerCloseAnim.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+const props = defineProps({
+  anim: {
+    type: Boolean,
+    default: false,
+  },
+  speed: {
+    type: Number,
+    default: 0.3,
+  },
+  delay: {
+    type: Number,
+    default: 0.2,
+  },
+})
+
+const { $gsap } = useNuxtApp()
+const tl = $gsap.timeline()
+
+const hbAnimRef = ref(null)
+const hbAnimRef1 = ref(null)
+const hbAnimRef2 = ref(null)
+const hbAnimRef3 = ref(null)
+
+const delay = props.delay
+const dur = props.speed
+onMounted(() => {
+  tl.pause()
+  tl.to(
+    [hbAnimRef1.value, hbAnimRef3.value],
+    {
+      delay: delay,
+      duration: dur,
+      ease: "power2.inOut",
+      y: (i) => (i === 0 ? -5.8 : 5.8), // Apply -5.8 to hbAnimRef1 and 5.8 to hbAnimRef3
+      x: 0,
+      transformOrigin: "50% 50%",
+    },
+    0 // Start both animations at the same time
+  )
+  tl.to(hbAnimRef1.value, {
+    duration: dur,
+    ease: "power2.inOut",
+    rotation: -45,
+    transformOrigin: "50% 50%",
+  })
+    .to(
+      hbAnimRef2.value,
+      {
+        duration: dur,
+        ease: "power2.inOut",
+        scaleX: 0,
+        transformOrigin: "50% 50%",
+      },
+      "<"
+    )
+    .to(
+      hbAnimRef3.value,
+      {
+        duration: dur,
+        ease: "power2.inOut",
+        rotation: 45,
+        transformOrigin: "50% 50%",
+      },
+      "<"
+    )
+
+  if (props.anim) {
+    tl.play()
+  }
+})
+</script>
+<template>
+  <svg
+    ref="hbAnimRef"
+    class="hb-anim relative"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    stroke="#fff"
+    stroke-width="1"
+    fill="rgba(0,0,0,0)"
+    stroke-linecap="round"
+    style="cursor: pointer; padding: 3px"
+  >
+    <path ref="hbAnimRef1" d="M4 18L20 18"></path>
+    <path ref="hbAnimRef2" d="M4 12L20 12"></path>
+    <path ref="hbAnimRef3" d="M4 6L20 6"></path>
+  </svg>
+</template>
