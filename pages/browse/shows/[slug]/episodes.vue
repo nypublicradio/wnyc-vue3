78a338ecce21a55a085d706b69cc204bf2176152
bonomite <thomasbono.com@gmail.com>
2025-07-17 17:46:30 -0400

    show episodes page created and reading the slug

---
+++ pages/browse/shows/[slug]/episodes.vue
@@ -0,0 +1,9 @@
+<script setup>
+const route = useRoute()
+const showSlug = computed(() => route.params.slug || "")
+</script>
+<template>
+  <section>
+    <h1>EPISODES PAGE: {{ showSlug }}</h1>
+  </section>
+</template>
