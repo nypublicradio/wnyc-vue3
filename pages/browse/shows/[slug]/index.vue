26a1b0ceaa5d23194bad8b702bf65da9839c49e3
Thomas Bono <thomasbono.com@gmail.com>
2025-07-25 13:49:23 -0400

    made ShowHeader component for show page and show episodes page

--- pages/browse/shows/[slug]/index.vue
+++ pages/browse/shows/[slug]/index.vue
@@ -1,31 +1,12 @@
 <script setup>
 import { useIntersectionObserver } from "@vueuse/core"
-import FollowIcon from "~/components/icons/FollowIcon.vue"
-import PlayIcon from "~/components/icons/PlayIcon.vue"
 
-import {
-  checkIsFavorited,
-  togglePlayEpisode,
-  trackClickEvent,
-  goToEpisodePage,
-  hasAudio,
-  addToFavorites2,
-  getEpisodeFallBackImage,
-} from "~/utilities/helpers"
-import {
-  useCurrentUser,
-  useIsEpisodePlaying,
-  useGlobalToast,
-  useIsApp,
-  useAppDownloadLink,
-} from "~/composables/states"
-import { mediaTypeRoutes, mediaTypes } from "~/composables/globals"
-import useSleepTimer from "~/composables/useSleepTimer"
+import { checkIsFavorited, trackClickEvent, goToEpisodePage } from "~/utilities/helpers"
+import { useGlobalToast, useIsApp } from "~/composables/states"
 
 const config = useRuntimeConfig()
 const route = useRoute()
 const router = useRouter()
-const appDownloadLink = useAppDownloadLink()
 
 const { data: show, status, error } = useFetch(
   `${config.public.BFF_URL}/api/v2/show/${route.params.slug}`
@@ -35,22 +16,13 @@ const page = ref(null)
 const episodes = ref(null)
 let maxPages = null
 
-// Computed properties derived from the show data
-const showImage = computed(() => show.value?.show?.image)
-const showTitle = computed(() => show.value?.show?.title)
-const showTease = computed(() => show.value?.show?.tease)
-const showScheduleSummary = computed(() => show.value?.show?.scheduleSummary)
 const showSlug = computed(() => show.value?.show?.slug)
 const isApp = useIsApp()
-const user = useCurrentUser()
-const isEpisodePlaying = useIsEpisodePlaying()
 
 const pendingMore = ref(false)
 const loadMoreRefVisible = ref(false)
 const loadMoreRef = ref(null)
 const isInitialObserver = ref(true)
-
-const { handleSleepTimer, sleepTimerRunning } = useSleepTimer()
 
 const { stop } = useIntersectionObserver(loadMoreRef, ([{ isIntersecting }]) => {
   // so it does not trigger on initial load and before we have data
@@ -100,58 +72,10 @@ const routeBack = () => {
   window.history.state.back ? router.go(-1) : navigateTo("/home")
 }
 
-// finds first episode with audio to play
-const firstEpisodeWithAudio = () => {
-  return episodes.value.find((ep) => {
-    if (hasAudio(ep.audio)) {
-      return ep
-    } else if (typeof ep.audio === "string") {
-      return ep
-    } else {
-      return null
-    }
-  })
-}
-// handle the toggle play button at the top to play the most recent episode with audio and tracking
-const togglePlayMostRecentEpisode = () => {
-  // handle NPR show segments.
-  if (show.value.show.cmsSource === "npr") {
-    // route to the first episode with a url parameter
-    navigateTo({
-      path: `${mediaTypeRoutes[mediaTypes.EPISODE]}${show.value.episodes.data[0].id}`,
-      query: {
-        src: "npr",
-        type: "episode",
-        autoplay: true,
-      },
-    })
-  } else {
-    const ep = firstEpisodeWithAudio()
-    togglePlayEpisode(ep)
-  }
-}
-
 // if user is logged in, check if item is already favorited
 const isFavorited = ref(false)
 watchEffect(async () => {
   isFavorited.value = await checkIsFavorited(route.params.slug)
-})
-
-// add item to favorites
-const handleAddToFavorites = () => {
-  // helper func for adding to favorites, also handles account prompt if not logged in
-  addToFavorites2({
-    item: show.value.show,
-    isFavorited: isFavorited.value,
-    message: "Updated your followed shows.",
-  })
-  if (user.value) {
-    isFavorited.value = !isFavorited.value
-  }
-}
-
-const hasEpisodes = computed(() => {
-  return episodes.value?.some((ep) => ep?.type !== "segment")
 })
 
 // navigate to the episodes page
@@ -228,174 +152,7 @@ onMounted(() => {
       <div class="grid">
         <div class="col-fixed hidden xxl:block w-20rem"></div>
         <div class="col">
-          <div class="flex justify-content-start gap-3 md:gap-5">
-            <VImage
-              v-if="showImage && status === 'success'"
-              :src="showImage"
-              :srcFallback="getEpisodeFallBackImage()"
-              :alt="`${showTitle} show image`"
-              :size="{ xs: [112, 112], md: [208, 208] }"
-              class="flex-none show-image w-7rem md:w-13rem"
-              :srcset="[2]"
-            />
-            <Skeleton
-              v-else
-              class="flex-none show-image w-7rem md:w-13rem h-7rem md:h-13rem"
-              borderRadius="0px"
-            />
-            <div
-              v-if="status === 'success'"
-              class="flex flex-column justify-content-start gap-3 mt-1 md:mt-2"
-            >
-              <h2 class="line-height-1 text-2xl md:text-6xl">{{ showTitle }}</h2>
-              <p v-if="showScheduleSummary" class="mt-0 md:-mt-3">
-                {{ showScheduleSummary }}
-              </p>
-              <HtmlConvert
-                v-if="showTease"
-                no-blocks
-                :htmlContent="showTease"
-                :key="`tease-${showSlug}`"
-                class="hidden md:block text-sm md:text-base"
-              />
-              <!-- desktop buttons -->
-              <div class="hidden md:flex align-items-center gap-3">
-                <Button
-                  class="play-btn flex-none"
-                  severity="secondary"
-                  rounded
-                  aria-label="play toggle"
-                  tabindex="0"
-                  :disabled="!hasEpisodes"
-                  @click="togglePlayMostRecentEpisode"
-                >
-                  <template #icon>
-                    <PauseIcon v-if="isEpisodePlaying" />
-                    <PlayIcon v-else />
-                  </template>
-                </Button>
-
-                <Button
-                  rounded
-                  severity="secondary"
-                  aria-label="follow"
-                  label="Follow"
-                  @click="handleAddToFavorites"
-                >
-                  <template #icon>
-                    <FollowIcon :active="isFavorited" style="height: 20px; width: 20px"
-                  /></template>
-                </Button>
-
-                <SleepTimerButton
-                  v-if="isApp"
-                  @emit-click="handleSleepTimer"
-                  :isActive="sleepTimerRunning"
-                  :isText="false"
-                  label="Sleep Timer"
-                  iconClass=""
-                  iconStyles="height: 20px; width: 20px;"
-                />
-                <Button
-                  v-else
-                  label="Listen in the app"
-                  severity="secondary"
-                  rounded
-                  class=""
-                  @click="
-                    navigateTo(appDownloadLink, {
-                      external: appDownloadLink.startsWith('http') ? true : false,
-                    })
-                  "
-                >
-                  <template #icon>
-                    <DevicesIcon style="height: 20px; width: 20px" />
-                  </template>
-                </Button>
-              </div>
-            </div>
-            <div v-else class="hidden md:flex flex-column gap-3 w-full">
-              <div class="flex flex-column gap-0">
-                <Skeleton class="my-2" height="48px" width="65%" borderRadius="24px" />
-                <Skeleton
-                  v-if="showScheduleSummary"
-                  height="14px"
-                  width="35%"
-                  borderRadius="24px"
-                />
-              </div>
-              <div class="flex flex-column gap-2">
-                <Skeleton height="14px" width="100%" borderRadius="24px" />
-                <Skeleton height="14px" width="100%" borderRadius="24px" />
-                <Skeleton height="14px" width="72%" borderRadius="24px" />
-              </div>
-              <div class="flex gap-3">
-                <Skeleton height="48px" width="48px" borderRadius="24px" />
-                <Skeleton height="41px" width="99px" borderRadius="24px" />
-                <Skeleton height="41px" width="178px" borderRadius="24px" />
-              </div>
-            </div>
-          </div>
-          <!-- mobile buttons -->
-          <div
-            v-if="status === 'success'"
-            class="flex md:hidden justify-content-center align-items-center gap-2 mt-3"
-          >
-            <Button rounded text plain aria-label="follow" @click="handleAddToFavorites">
-              <template #icon>
-                <FollowIcon :active="isFavorited" class="w-2rem mt-1"
-              /></template>
-            </Button>
-
-            <Button
-              class="play-btn flex-none"
-              severity="secondary"
-              rounded
-              aria-label="play toggle"
-              tabindex="0"
-              :disabled="!hasEpisodes"
-              @click="togglePlayMostRecentEpisode"
-            >
-              <template #icon>
-                <PauseIcon v-if="isEpisodePlaying" />
-                <PlayIcon v-else />
-              </template>
-            </Button>
-
-            <SleepTimerButton
-              v-if="isApp"
-              @emit-click="handleSleepTimer"
-              :isActive="sleepTimerRunning"
-              :isText="true"
-              class="mt-1"
-            />
-            <Button
-              v-else
-              label=""
-              severity="secondary"
-              rounded
-              text
-              plain
-              class=""
-              @click="
-                navigateTo(appDownloadLink, {
-                  external: appDownloadLink.startsWith('http') ? true : false,
-                })
-              "
-            >
-              <template #icon>
-                <DevicesIcon class="h-2rem w-2rem mt-1" />
-              </template>
-            </Button>
-          </div>
-          <div
-            v-else
-            class="flex md:hidden justify-content-center align-items-center gap-2 mt-3"
-          >
-            <Skeleton height="37px" width="37px" borderRadius="20px" />
-            <Skeleton height="48px" width="48px" borderRadius="24px" />
-            <Skeleton height="37px" width="37px" borderRadius="20px" />
-          </div>
+          <ShowHeader :show="show" />
         </div>
         <div class="col-fixed hidden lg:block w-20rem"></div>
       </div>
