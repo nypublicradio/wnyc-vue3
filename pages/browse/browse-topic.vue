7169c549d30bf39b81938ad7cfd99f4c8bc358b0
Thomas Bono <thomasbono.com@gmail.com>
2025-08-01 14:20:41 -0400

    browse-topic page updated

--- pages/browse/browse-topic.vue
+++ pages/browse/browse-topic.vue
@@ -1,8 +1,12 @@
 <script setup>
-const router = useRouter()
+import { goToShowPage } from "~/utilities/helpers"
+import { useBreakpoints } from "~/composables/useBreakpoints"
+
 const route = useRoute()
 const config = useRuntimeConfig()
-const { data: categoryData, pending, error } = useFetch(
+const { breakpoint } = useBreakpoints()
+const isMobile = computed(() => breakpoint("<md"))
+const { data: categoryData, status, error } = useFetch(
   `${config.public.BFF_URL}/api/browse/browse-topic/getTopicData`,
   {
     params: {
@@ -12,22 +16,16 @@ const { data: categoryData, pending, error } = useFetch(
 )
 // navigate back to home and track it
 const backHome = () => {
-  router.go(-1)
+  navigateTo("/browse")
 }
 
 const label = route.query.label
-
-const goToShowPage = (show) => {
-  navigateTo({
-    path: `shows/${show.slug}`,
-  })
-}
 
 onMounted(() => {
   // send GA page view
   const { $analytics } = useNuxtApp()
   $analytics.sendPageView({
-    page_title: "Browse Topics",
+    page_title: `Browse Topics - ${route.query.topic}`,
     page_type: "browse_topics_tab",
     content_group: "app_tab",
   })
@@ -56,7 +54,7 @@ onUnmounted(() => {
       </Head>
     </Html>
     <section class="">
-      <div class="flex">
+      <div class="flex align-items-center">
         <Button
           class="back-btn text-color -ml-3"
           icon="pi pi-chevron-left"
@@ -69,23 +67,39 @@ onUnmounted(() => {
         <h1>{{ label }}</h1>
       </div>
     </section>
-    <section class="shows flex flex-column gap-5">
-      <FetchError v-if="error" />
-      <template v-if="!pending">
-        <!-- data = {{ categoryData }} -->
+    <FetchError v-if="error" />
+    <section class="grid">
+      <template v-if="status === 'success'">
         <ShowItem
           v-for="show in categoryData"
           :data="show"
           :key="show.title"
+          class="col-12 md:col-4 md:mb-5"
+          rootClass="md:align-items-start"
+          contentClass="md:flex-column gap-3 md:gap-2"
+          imageClass="w-7rem md:w-13rem"
+          :size="{ xs: [112, 112], md: [208, 208] }"
+          :hideButtons="!isMobile"
           @onClick="goToShowPage(show)"
         />
       </template>
-      <skeleton-show-item v-else v-for="(show, index) in 27" :key="`sk1-${index}`" />
+
+      <skeleton-show-item
+        v-else
+        v-for="(show, index) in 15"
+        :key="`sk1-${index}`"
+        class="col-12 md:col-4 md:mb-5"
+        contentClass="md:flex-column gap-3 md:gap-2"
+        imageClass="w-7rem md:w-13rem h-7rem md:h-13rem"
+        :hideButtons="!isMobile"
+      />
     </section>
   </div>
 </template>
 
 <style lang="scss">
 .browse-topic-page {
+  max-width: $thinContentWidth;
+  margin: auto;
 }
 </style>
