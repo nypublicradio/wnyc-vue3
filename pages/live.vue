4472229324d808f5c33f63a6888c6feb6a2d6327
Thomas Bono <thomasbono.com@gmail.com>
2025-08-14 17:55:12 -0400

    schedule continue

--- pages/live.vue
+++ pages/live.vue
@@ -224,76 +224,90 @@ onUnmounted(() => {
     </div>
     <!-- <pre class="text-xs overflow-hidden">{{ allCurrentStations }}</pre> -->
     <!-- <pre>{{ liveScheduleData }}</pre> -->
-    <section class="current-station-info grid grid-nogutter m-auto">
-      <div class="col pr-2 lg:pr-4">TAB CONTENT</div>
-      <div class="col-fixed hidden xl:block xl:w-20rem">AD</div>
-    </section>
-    <section class="schedule">
-      <h2 class="text-5xl">Schedule</h2>
-      <div v-if="liveScheduleData">
-        <div
-          v-for="(entry, index) in liveScheduleData"
-          :key="`${entry.id}-${index}`"
-          class="schedule-entry flex justify-content-between align-items-center gap-3 mt-4"
-          :class="[{ selected: index === 0 }]"
-        >
-          <div class="flex align-items-stretch">
-            <div class="left my-1" />
-            <div>
-              <p class="time">
-                {{ getTheTime(entry.attributes.start, entry.attributes.end, index) }}
-              </p>
-              <h2 class="title">
-                {{ getEntryTitle(entry) }}
-              </h2>
-            </div>
-          </div>
+    <section class="schedule grid m-auto">
+      <div class="col pr-2 lg:pr-4">
+        <div class="flex flex-wrap justify-content-between align-items-end mb-4">
+          <h2 class="text-5xl">Schedule</h2>
           <Button
-            v-if="isApp && index > 0"
             severity="secondary"
-            text
-            plain
-            rounded
-            class="flex-none"
-            aria-label="set notification"
-            @click="handleScheduleLocalNotification(entry)"
+            variant="link"
+            class="link -ml-2"
+            @click="handleAllTopics"
+            label="Weekly Schedule (pdf)"
+          ></Button>
+        </div>
+        <div v-if="liveScheduleData">
+          <div
+            v-for="(entry, index) in liveScheduleData"
+            :key="`${entry.id}-${index}`"
+            class="schedule-entry flex justify-content-between align-items-center gap-3 mt-4"
+            :class="[{ selected: index === 0 }]"
           >
-            <template #icon>
-              <NotificationIcon :entry="entry" />
-            </template>
-          </Button>
+            <div class="flex align-items-stretch">
+              <div class="left my-1" />
+              <div>
+                <p class="time">
+                  {{ getTheTime(entry.attributes.start, entry.attributes.end, index) }}
+                </p>
+                <h2 class="title">
+                  {{ getEntryTitle(entry) }}
+                </h2>
+              </div>
+            </div>
+            <Button
+              v-if="isApp && index > 0"
+              severity="secondary"
+              text
+              plain
+              rounded
+              class="flex-none"
+              aria-label="set notification"
+              @click="handleScheduleLocalNotification(entry)"
+            >
+              <template #icon>
+                <NotificationIcon :entry="entry" />
+              </template>
+            </Button>
+          </div>
+        </div>
+        <div v-else class="skeleton mt-5">
+          <div
+            v-for="i in 10"
+            :key="`schedule-skeleton-${i}`"
+            class="flex align-items-center justify-content-between pr-2 mb-5"
+          >
+            <div class="flex gap-3">
+              <Skeleton
+                height="30px"
+                width="4px"
+                borderRadius="2px"
+                :class="[{ 'opacity-0': i > 0 }]"
+              />
+              <div class="flex flex-column gap-1">
+                <Skeleton
+                  class="opacity-50"
+                  height="12px"
+                  width="64px"
+                  borderRadius="4px"
+                />
+                <Skeleton height="14px" width="174px" borderRadius="4px" />
+              </div>
+            </div>
+            <Skeleton
+              :class="[{ 'opacity-0': i < 1 }]"
+              height="26px"
+              width="26px"
+              borderRadius="15px"
+            />
+          </div>
         </div>
       </div>
-      <div v-else class="skeleton mt-5">
-        <div
-          v-for="i in 10"
-          :key="`schedule-skeleton-${i}`"
-          class="flex align-items-center justify-content-between pr-2 mb-5"
-        >
-          <div class="flex gap-3">
-            <Skeleton
-              height="30px"
-              width="4px"
-              borderRadius="2px"
-              :class="[{ 'opacity-0': i > 0 }]"
-            />
-            <div class="flex flex-column gap-1">
-              <Skeleton
-                class="opacity-50"
-                height="12px"
-                width="64px"
-                borderRadius="4px"
-              />
-              <Skeleton height="14px" width="174px" borderRadius="4px" />
-            </div>
-          </div>
-          <Skeleton
-            :class="[{ 'opacity-0': i < 1 }]"
-            height="26px"
-            width="26px"
-            borderRadius="15px"
-          />
-        </div>
+      <div class="col-fixed hidden xl:block xl:w-20rem justify-content-center">
+        <story-htlAd
+          layout="rectangle"
+          slotClass="htlad-wnyc_livepage_rectangle"
+          fineprint="WNYC is funded by sponsors and member donations"
+        />
       </div>
     </section>
   </div>
