3275a339a643f1045506bddd6e42c341b75a1793
Thomas Bono <thomasbono.com@gmail.com>
2025-09-04 20:09:20 -0400

    member center card views complete

--- components/MemberStatusIcon.vue
+++ components/MemberStatusIcon.vue
@@ -1,7 +1,18 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+  brand: {
+    type: String,
+    default: "wnyc",
+  },
+  status: {
+    type: String,
+    default: "check",
+  },
+})
+</script>
 <template>
   <img
-    src="/member-status.svg"
+    :src="`/member-status-icon/${props.brand}-${props.status}.svg`"
     alt="Membership certificate icon"
     class="max-w-4rem h-auto"
   />
