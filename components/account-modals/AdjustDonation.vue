4201e44d5e0a6986ea72b5ae0c24fc262abee4c3
Thomas Bono <thomasbono.com@gmail.com>
2025-09-06 20:59:36 -0400

    setting up complicated radio button form

--- components/account-modals/AdjustDonation.vue
+++ components/account-modals/AdjustDonation.vue
@@ -16,8 +16,28 @@ const onAdjust = () => {
   emit("adjust")
 }
 
-const value = ref("$15/mo")
-const options = ref(["$15/mo", "$30/mo", "$34/mo", "other"])
+const InputNumberRef = ref(null)
+
+const finalAmount = ref(15)
+const value = ref(15)
+const otherAmount = ref(null)
+const options = ref([15, 30, 45, "other"])
+
+const onUpdateOtherAmount = (e) => {
+  console.log("updating other amount = ", e.value)
+  finalAmount.value = e.value
+}
+const onRbClicked = (e) => {
+  console.log("clicked RB", e)
+  value.value = e
+  finalAmount.value = e
+  if (e === "other") {
+    console.log("is other")
+    // set focus to the inputtext element after nextTick()
+
+    InputNumberRef.value.focus()
+  }
+}
 
 onMounted(() => {
   currentDonationAmount.value = dialogRef.value.data.currentDonationAmount
@@ -38,17 +58,36 @@ onMounted(() => {
       />
     </div>
     <p>Your current monthly gift is {{ formatCurrency(currentDonationAmount) }}.</p>
-
+    {{ value }}
+    {{ finalAmount }}
     <div class="amount-rb flex align-items-center gap-2 my-4">
       <Button
         v-for="amount in options"
         class="px-3"
-        :label="amount"
         severity="secondary"
         size="small"
-        @click="value = amount"
-        :class="value === amount ? 'selected' : ''"
-      />
+        @click="onRbClicked(amount)"
+        :class="[{ selected: value === amount }, { other: value === 'other' }]"
+      >
+        <p v-if="amount !== 'other'" class="font-bold">${{ amount }}/mo</p>
+        <p v-else-if="amount === 'other' && value !== amount" class="font-bold">
+          $ Other
+        </p>
+
+        <InputNumber
+          v-else
+          ref="InputNumberRef"
+          prefix="$"
+          suffix="/mo"
+          v-model="otherAmount"
+          inputId="minmaxfraction"
+          :minFractionDigits="2"
+          :maxFractionDigits="5"
+          placeholder="$0.00/mo"
+          fluid
+          @value-change="onUpdateOtherAmount"
+        />
+      </Button>
     </div>
 
     <div class="flex flex-column gap-3 align-items-center mt-3">
@@ -69,14 +108,29 @@ onMounted(() => {
   </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .adjust-donation {
   .amount-rb {
     .p-button {
+      border-color: #000000;
       &.selected {
         background-color: #000000;
         border-color: transparent;
-        color: #fff;
+        p {
+          color: #fff;
+        }
+        &.other {
+        }
+      }
+      .p-inputnumber {
+        .p-inputtext {
+          max-width: 110px;
+          padding: 0;
+          background-color: transparent;
+          border: transparent;
+          font-weight: 700;
+          color: #ffffff;
+        }
       }
     }
   }
