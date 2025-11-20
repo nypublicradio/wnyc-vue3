447ba482ed2d6efe5360b8b7f1f02809842fd6f1
Thomas Bono <thomasbono.com@gmail.com>
2025-11-20 17:09:58 -0500

    events page set up for mediaCard now. just need to see the actual layout of the page

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,36 +1,28 @@
 <script setup lang="js">
 
 import { useToast } from "primevue/usetoast"
-import { togglePlayEpisode } from "~/utilities/helpers"
 import { useTopStories } from "~/composables/useTopStories"
-import EpisodeTemplate from "~/components/EpisodeTemplate.vue"
-const { getFilteredTopStories } = useTopStories()
+const { getFilteredTopStories, topStories } = useTopStories()
 const { $analytics } = useNuxtApp()
 const config = useRuntimeConfig()
-const route = useRoute()
-const router = useRouter()
 const toast = useToast()
 
-const { data: event, status, error } = useFetch(
+const { data: events, status, error } = useFetch(
   `${config.public.BFF_URL}/api/events/list`,
   {
     onResponse({ response }) {
       const res = response._data
       console.log("event root response:", res)
       $analytics.sendPageView({
-        page_title: res.title,
-        page_type: "event_page",
-        content_group: "on_demand_event",
-        article_authors: res?.authors?.map((author) => author.name).join(","),
-        article_publish_date: res.publicationDate,
-        article_updated_date: res.updatedDate ? res.updatedDate : res.publicationDate,
-        article_title: res.title,
+        page_title: "Events Page",
+        page_type: "events_page",
+        content_group: "events",
       })
     },
     onResponseError() {
       toast.add({
         severity: "error",
-        summary: "We are having a problem loading this event. Please try again later.",
+        summary: "We are having a problem loading these events. Please try again later.",
         life: 6000,
         closable: true,
       })
@@ -38,36 +30,21 @@ const { data: event, status, error } = useFetch(
   }
 )
 
-// const eventData = computed(() => event.value)
-
-// const theSlug = computed(
-//   () =>
-//     eventData.value?.showSlug ||
-//     eventData.value?.show ||
-//     eventData.value?.headers.brand.slug
-// )
-
-// const theShowTitle = computed(
-//   () =>
-//     eventData.value?.showTitle ||
-//     eventData.value?.headers.brand.title ||
-//     eventData.value?.title
-// )
+const eventData = computed(() => events.value)
 
 const breadcrumbs = computed(() => [
   { label: "Home", route: "/home" },
   { label: "Events", route: "/events" },
-  { label: theShowTitle.value, route: `/events/${theSlug.value}` },
 ])
 </script>
 
 <template>
   <div class="event-page">
-    <!-- <Html lang="en">
+    <Html lang="en">
       <Head>
-        <Title>{{ eventData?.title }} | WNYC</Title>
-        <Meta name="og:title" :content="`${eventData?.title} | WNYC`" />
-        <Meta name="twitter:title" :content="`${eventData?.title} | WNYC`" />
+        <Title>Events | WNYC</Title>
+        <!-- <Meta name="og:title" :content="`${eventData?.title} | WNYC`" />
+        <Meta name="twitter:title" :content="`${eventData?.title} | WNYC`" /> -->
       </Head>
     </Html>
     <section>
@@ -76,17 +53,18 @@ const breadcrumbs = computed(() => [
       </div>
     </section>
     <FetchError v-if="error" />
-    <FetchError v-if="showError" />
+
     <section class="py-6">
-      <h1>This Event</h1>
+      <h1>Events</h1>
+      <pre>{{ eventData.events }}</pre>
     </section>
 
     <section v-if="getFilteredTopStories">
       <Divider class="mt-2 mb-5" />
       <h2 class="mb-3">Top Stories From Gothamist</h2>
-      <TopStories :articles="getFilteredTopStories(episodeData)" />
+      <TopStories :articles="topStories" />
     </section>
 
-    <BackToTopButton />-->
+    <BackToTopButton />
   </div>
 </template>
