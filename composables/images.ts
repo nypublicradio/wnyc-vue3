454c23a7744a58be367578a2f418e8aa1215f96b
Kim LaRocca <kim@kimlarocca.com>
2025-12-02 14:02:51 -0500

    ok we don't need it at all!

--- composables/images.ts
+++
@@ -1,7 +0,0 @@
-import type Image from './types/Image'
-
-// calculate quality based on size multiplier
-function calcQuality (quality, size) {
-    const qual = size >= 2 ? quality - Math.round(size * 5) : quality
-    return qual >= 15 ? qual : 15
-}
