1e7ec7e9d4d875d764a5d652f1ef1c693bcee56b
Thomas Bono <thomasbono.com@gmail.com>
2025-12-04 18:00:29 -0500

    page updated

--- pages/ask-the-mayor.vue
+++ pages/ask-the-mayor.vue
@@ -1,13 +1,39 @@
-<template>
-  <div>
-    
-  </div>
-</template>
 
-<script lang="ts" setup>
-
+<script setup>
+onMounted(() => {
+  // send GA page view
+  const { $analytics } = useNuxtApp();
+  $analytics.sendPageView({
+    page_title: "Ask the Mayor",
+    page_type: "ask_the_mayor",
+    content_group: "ask_the_mayor",
+  });
+});
 </script>
 
-<style>
+<template>
+  <div>
+    <Html lang="en">
+      <Head>
+        <Title
+          >WNYC | New York Public Radio, Podcasts, Live Streaming Radio,
+          News</Title
+        >
+        <Meta
+          name="og:title"
+          content="WNYC | New York Public Radio, Podcasts, Live Streaming Radio, News"
+        />
+        <Meta
+          name="twitter:title"
+          content="WNYC | New York Public Radio, Podcasts, Live Streaming Radio, News"
+        />
+      </Head>
+    </Html>
+  </div>
+  <section>
+    <h1>Ask the Mayor</h1>
+  </section>
+</template>
 
+<style>
 </style>
