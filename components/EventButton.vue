8dfbdf80a224e044c2f2803071266a3505f27a40
Thomas Bono <thomasbono.com@gmail.com>
2025-12-04 15:11:56 -0500

    event card updated with location time and buttons and tags

---
+++ components/EventButton.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+const props = defineProps({
+  label: {
+    type: String,
+    default: "Get Tickets",
+  },
+  live: {
+    type: Boolean,
+    default: false,
+  },
+  isDownloaded: {
+    type: Boolean,
+    default: false,
+  },
+  progress: {
+    type: Number,
+    default: 0,
+  },
+  /* file filed to match up agaist or, if playing a downloaded file, the directoryAudio.name to match up against */
+  file: {
+    default: "",
+    type: String,
+  },
+})
+
+const emit = defineEmits(["on-click"])
+</script>
+
+<template>
+  <div class="read-button" :class="[{ circle: props.label === '' }]">
+    <Button
+      severity="secondary"
+      @click="emit('on-click')"
+      aria-label="read this article"
+      class="flex align-items-center cursor-pointer"
+    >
+      <slot name="icon">
+        <div class="flex align-items-center icon">
+          <i class="pi pi-ticket" />
+        </div>
+      </slot>
+      <slot>
+        <div class="content flex white-space-nowrap">
+          <span>{{ props.label }}</span>
+        </div>
+      </slot>
+    </Button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.read-button {
+  .p-button {
+    padding: 0.219rem 0.75rem;
+    min-height: 33px;
+    &.active {
+      //border: var(--bw-toggle) 1px solid;
+    }
+  }
+  .content {
+    font-size: 14px;
+    font-weight: var(--font-weight-700);
+    line-height: normal;
+    align-items: center;
+    * {
+      line-height: 1;
+    }
+  }
+}
+</style>
