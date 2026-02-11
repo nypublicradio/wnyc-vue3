4d9b3d61690ed1be2854b6fdb6fa3c3c479599a6
Kim LaRocca <kim@kimlarocca.com>
2026-02-11 17:26:28 -0500

    add some styles to the information page template

--- components/InformationPageTemplate.vue
+++ components/InformationPageTemplate.vue
@@ -8,7 +8,7 @@ defineProps<{
 
 <template>
   <div>
-    <section v-if="page" class="section-page mb-5">
+    <section v-if="page" class="information-page mb-5">
       <div class="content">
         <h1 class="mb-5">
           {{ page.title }}
@@ -20,3 +20,12 @@ defineProps<{
     </section>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.information-page {
+  .content {
+    max-width: 614px;
+    margin: 0 auto;
+  }
+}
+</style>
