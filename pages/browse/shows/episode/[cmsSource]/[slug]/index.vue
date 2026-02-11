1479a0008a4de81cafa000f1366905ab5c7d2d20
Wilfredo Colon <wcolon@nypublicradio.org>
2026-02-11 11:36:41 -0500

    Add Simplecast support and refactor episode handling across component… (#445)
    * Add Simplecast support and refactor episode handling across components and API
    
    * Refactor audio and image handling in normalize functions for consistency
    
    * Add SIMPLECAST_API_KEY to build arguments in review app workflow
    
    * Enhance Simplecast API error handling and logging in show and episode retrieval functions
    
    * Add SIMPLECAST_URL argument to Dockerfile and workflow for environment configuration
    
    * Add showSlug to Simplecast normalization and enhance episode data extraction
    
    * Update Simplecast episode API authorization to use environment variable for API key
    
    * Add fallback to use process.env.SIMPLECAST_API_KEY with detailed logging
    
    * Fix null keyword handling in Simplecast normalization
    
    * Add process.env fallback for SIMPLECAST_API_KEY in show endpoint
    
    * Clean up diagnostic console.log statements
    
    * Handle Simplecast images with URL property in EpisodeTemplate and useVImage
    
    * Simplify URL validation logic for Simplecast images in useVImage
    
    * Refactor goToEpisodePage to use cmsSources.PUBLISHER for default cmsSource
    
    * Enhance normalization functions to prioritize subtitle over description for tease and listingDescription
    
    * Refactor cmsSource assignment to use cmsSources constants for consistency
    
    * Refactor normalizeSimplecastPage to use dynamic data type and improve consistency
    
    * Refactor normalizeSimplecastPage to simplify promise resolution
    
    * Refactor import statement for cmsSources to use globals for consistency
    
    * Refactor normalizeSimplecastPage to use SimplecastArticle interface for improved type safety and consistency
    
    * Refactor normalizeSimplecastPage to utilize helper functions for improved readability and maintainability

---
+++ pages/browse/shows/episode/[cmsSource]/[slug]/index.vue
@@ -0,0 +1,148 @@
+<script setup>
+import { useToast } from "primevue/usetoast"
+import { togglePlayEpisode } from "~/utilities/helpers"
+import { useTopStories } from "~/composables/useTopStories"
+import EpisodeTemplate from "~/components/EpisodeTemplate.vue"
+const { getFilteredTopStories } = useTopStories()
+const { $analytics } = useNuxtApp()
+const config = useRuntimeConfig()
+const route = useRoute()
+const router = useRouter()
+const toast = useToast()
+
+const {
+  data: episode,
+  status,
+  error,
+} = useFetch(
+  () => `${config.public.BFF_URL}/api/v2/show/episode/${route.params.cmsSource}/${route.params.slug}`,
+  {
+    onResponse({ response }) {
+      const res = response._data
+      $analytics.sendPageView({
+        page_title: res.title,
+        page_type: "episode_page",
+        content_group: "on_demand_episode",
+        article_authors: res?.authors?.map((author) => author.name).join(","),
+        article_publish_date: res.publicationDate,
+        article_updated_date: res.updatedDate
+          ? res.updatedDate
+          : res.publicationDate,
+        article_title: res.title,
+      })
+
+      // check route param autoplay exists and if so, play the first segment
+      if (route.query.autoplay === "true") {
+        togglePlayEpisode(res.audio[0])
+        // remove the autoplay query param
+        router.replace({ query: { ...route.query, autoplay: null } })
+      }
+    },
+    onResponseError() {
+      toast.add({
+        severity: "error",
+        summary:
+          "We are having a problem loading this episode. Please try again later.",
+        life: 6000,
+        closable: true,
+      })
+    },
+  }
+)
+
+const episodeData = computed(() => episode.value)
+
+const theSlug = computed(
+  () =>
+    episodeData.value?.showSlug ||
+    episodeData.value?.showId ||
+    episodeData.value?.show?.slug ||
+    episodeData.value?.headers?.brand?.slug
+)
+
+const theShowTitle = computed(
+  () =>
+    episodeData.value?.showTitle ||
+    episodeData.value?.headers.brand.title ||
+    episodeData.value?.title
+)
+
+const {
+  data: show,
+  status: showStatus,
+  error: showError,
+  execute: executeShowFetch,
+} = useLazyFetch(
+  () => `${config.public.BFF_URL}/api/v2/show/${theSlug.value}`,
+  {
+    immediate: false,
+    server: false,
+  }
+)
+
+const breadcrumbs = computed(() => [
+  { label: "Home", route: "/home" },
+  { label: "Browse", route: "/browse" },
+  { label: theShowTitle.value, route: `/browse/shows/${theSlug.value}` },
+])
+
+watch(
+  status,
+  () => {
+    if (status.value === "success" && theSlug.value) {
+      executeShowFetch()
+    }
+  },
+  { immediate: false }
+)
+</script>
+
+<template>
+  <div class="episode-page">
+    <Html lang="en">
+      <Head>
+        <Title>{{ episodeData?.title }} | WNYC</Title>
+        <Meta name="og:title" :content="`${episodeData?.title} | WNYC`" />
+        <Meta name="twitter:title" :content="`${episodeData?.title} | WNYC`" />
+      </Head>
+    </Html>
+    <section>
+      <div class="flex align-items-center">
+        <Breadcrumbs :items="breadcrumbs" />
+      </div>
+    </section>
+    <FetchError v-if="error" />
+    <FetchError v-if="showError" />
+
+    <EpisodeTemplate
+      :pending="status !== 'success'"
+      :episodeData="episodeData"
+      :show="show"
+      :showPending="showStatus !== 'success'"
+    >
+      <template #bottom>
+        <Divider class="mt-8 mb-5" />
+        <h2 class="mb-3">Top Stories From Gothamist</h2>
+        <TopStories :articles="getFilteredTopStories(episodeData)" />
+      </template>
+    </EpisodeTemplate>
+
+    <BackToTopButton />
+  </div>
+</template>
+
+<style lang="scss">
+.episode-page .segment-list .beforeHack {
+  &::before {
+    content: "";
+    display: block;
+    height: 0px;
+  }
+}
+
+.episode-page .html-convert {
+  p {
+    line-height: 1.8em;
+  }
+}
+</style>
