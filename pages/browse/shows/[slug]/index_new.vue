5403175c7c9bf1a3913e1ee117c004d12118f566
Thomas Bono <thomasbono.com@gmail.com>
2026-02-25 10:52:45 -0500

    push to main-org

--- pages/browse/shows/[slug]/index_new.vue
+++ pages/browse/shows/[slug]/index_new.vue
@@ -14,12 +14,7 @@ const {
   data: show,
   status,
   error,
-} = useFetch(`${config.public.BFF_URL}/api/v3/show/${route.params.slug}`)
-// const {
-//   data: show,
-//   status,
-//   error,
-// } = useFetch(`${config.public.BFF_URL}/api/pages/wagtail/${route.params.slug}`)
+} = useFetch(`${config.public.BFF_URL}/api/pages/wagtail/${route.params.slug}`)
 
 const page = ref(null)
 const episodes = ref([])
@@ -188,7 +183,7 @@ onUnmounted(() => {
       </div>
       <FetchError v-if="error" />
     </section>
-    <pre>{{ show }}</pre>
+    <!-- <pre>{{ show }}</pre> -->
     <ShowHeader :show="show" />
 
     <!-- JUMP LINKS -->
