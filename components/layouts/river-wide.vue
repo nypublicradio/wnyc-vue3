b31c28977f8c6c53abd059f835c4d01cc19b3dd2
Thomas Bono <thomasbono.com@gmail.com>
2026-02-26 14:02:54 -0500

    try catch on finding the layout component to default to river

---
+++ components/layouts/river-wide.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  list: {
+    type: Object,
+    required: true,
+  },
+  cardClass: {
+    type: String,
+    default: "col-12 lg:col-6",
+  },
+})
+</script>
+
+<template>
+  <river :list="props.list" :cardClass="props.cardClass" />
+</template>
