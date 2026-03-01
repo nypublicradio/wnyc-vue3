9a7c9912742d4f22699753fb48c5b70c2979640f
Thomas Bono <thomasbono.com@gmail.com>
2026-03-01 17:17:26 -0500

    starting cta block

---
+++ components/streamfield/ctaBlock.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { ctaBlock } from "../../composables/types/StreamfieldBlock"
+
+defineProps<{
+  block: ctaBlock
+}>()
+</script>
+
+<template>
+  <div class="cta-block">
+    ctaBlock
+    <pre>{{ block.value }}</pre>
+    <!-- <VImage
+      v-if="block.value.image"
+      :src="block.value.image"
+      :size="{
+        xxs: [316, 210],
+        xs: [517, 344],
+        sm: [709, 472],
+        md: [885, 589],
+        lg: [757, 504],
+        xl: [923, 614],
+        xxl: [688, 458],
+      }"
+      :maxHeight="block.value.image.height"
+      :maxWidth="block.value.image.width"
+      allowVerticalEffect
+      :alt="block.value.image.alt"
+    >
+      <template #caption>
+        <VImageCaption
+          v-if="block.value.caption || block.value.image.caption"
+          :text="block.value.caption || block.value.image.caption"
+        />
+      </template>
+    </VImage> -->
+  </div>
+</template>
