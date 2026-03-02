7a622893235cb48b15e1b0c2446c10304872c8c0
Thomas Bono <thomasbono.com@gmail.com>
2026-03-02 14:41:04 -0500

    CTA Block looking good

--- components/streamfield/ctaBlock.vue
+++ components/streamfield/ctaBlock.vue
@@ -1,38 +1,59 @@
 <script setup lang="ts">
 import type { ctaBlock } from "../../composables/types/StreamfieldBlock"
 
-defineProps<{
+const props = defineProps<{
   block: ctaBlock
 }>()
+const ratio = [
+  props.block.value.image.height || 3,
+  props.block.value.image.width || 2,
+]
+
+const widthsObj = {
+  xxs: 316,
+  xs: 517,
+  sm: 709,
+  md: 885,
+  lg: 923,
+  xl: 610,
+}
+const getRoute = (url: string) => {
+  if (!url) return url
+  try {
+    const parsedUrl = new URL(url)
+    const internalRouteDomains = [
+      "www.wnyc.org",
+      "demo.wnyc.org",
+      "www.demo.wnyc.org",
+    ]
+    if (internalRouteDomains.includes(parsedUrl.hostname)) {
+      return parsedUrl.pathname + parsedUrl.search + parsedUrl.hash
+    }
+  } catch (e) {
+    // Ignore invalid URL errors, might already be a relative path
+  }
+  return url
+}
+
+const link = getRoute(props.block.value.url)
+const image = props.block.value.image
+
+const getHeight = (w: number) => Math.round((w * ratio[0]) / ratio[1])
+
+const ctaImageSize = Object.fromEntries(
+  Object.entries(widthsObj).map(([k, w]) => [k, [w, getHeight(w)]])
+) as Record<keyof typeof widthsObj, [number, number]>
 </script>
 
 <template>
-  <div class="cta-block">
-    ctaBlock
-    <pre>{{ block.value }}</pre>
-    <!-- <VImage
-      v-if="block.value.image"
-      :src="block.value.image"
-      :size="{
-        xxs: [316, 210],
-        xs: [517, 344],
-        sm: [709, 472],
-        md: [885, 589],
-        lg: [757, 504],
-        xl: [923, 614],
-        xxl: [688, 458],
-      }"
-      :maxHeight="block.value.image.height"
-      :maxWidth="block.value.image.width"
-      allowVerticalEffect
-      :alt="block.value.image.alt"
-    >
-      <template #caption>
-        <VImageCaption
-          v-if="block.value.caption || block.value.image.caption"
-          :text="block.value.caption || block.value.image.caption"
-        />
-      </template>
-    </VImage> -->
-  </div>
+  <VFlexibleLink :to="link" v-if="image && link">
+    <VImage
+      :src="image"
+      :size="ctaImageSize"
+      :maxHeight="image.height"
+      :maxWidth="image.width"
+      :allowVerticalEffect="false"
+      :alt="`${props.block.type}-${props.block.id}-image`"
+    />
+  </VFlexibleLink>
 </template>
