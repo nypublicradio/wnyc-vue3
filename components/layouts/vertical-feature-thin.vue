197ed861bb20fbd437ef8dbcc137e517c98d333f
Thomas Bono <thomasbono.com@gmail.com>
2026-03-06 18:45:47 -0500

    thin layouts done

---
+++ components/layouts/vertical-feature-thin.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  list: {
+    type: Object,
+    required: true,
+  },
+  maxItems: {
+    type: Number,
+    default: null,
+  },
+  seeMore: {
+    type: Object,
+    default: null,
+    required: false,
+  },
+  isThin: {
+    type: Boolean,
+    default: true,
+  },
+})
+</script>
+
+<template>
+  <layouts-vertical-feature
+    :is-thin="props.isThin"
+    :label="props.label"
+    :list="props.list"
+    :max-items="props.maxItems"
+    :see-more="props.seeMore"
+  />
+</template>
+
