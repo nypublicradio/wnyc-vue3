afee3d24c0e9bb95164849a718982c66c370aa75
Thomas Bono <thomasbono.com@gmail.com>
2026-03-06 18:45:47 -0500

    thin layouts done

---
+++ components/layouts/text-only-thin.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { dynamicNavigation } from "~/utilities/helpers"
+const props = defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  list: {
+    type: Object,
+    required: true,
+  },
+  cardClass: {
+    type: String,
+    default: "col-12",
+  },
+  seeMore: {
+    type: Object,
+    default: null,
+    required: false,
+  },
+})
+</script>
+
+<template>
+  <layouts-text-only
+    :label="props.label"
+    :list="props.list"
+    :cardClass="props.cardClass"
+    :seeMore="props.seeMore"
+  />
+</template>
