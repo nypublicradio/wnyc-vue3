744504116f10215777588ae25340c082ceb0fa4e
walsh9 <walsh9@gmail.com>
2026-03-09 19:47:46 -0400

    remove empty placeholder templates

--- pages/contact.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <section class="py-6">
-    <h1>Contact</h1>
-  </section>
-</template>
--- pages/newsletter.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <section class="privacy-page">
-    <h1>Newsletter</h1>
-  </section>
-</template>
