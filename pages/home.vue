075562646003e449d303e3535f01e2fa3fb19c83
Thomas Bono <thomasbono.com@gmail.com>
2026-03-10 18:38:37 -0400

    removed comment

--- pages/home.vue
+++ pages/home.vue
@@ -77,7 +77,7 @@ onMounted(() => {
       </div>
       <!-- <VImage src="/fallback-ep.png" /> -->
     </section>
-    <pre>{{ pagedata?.new_home_template.curatedContent }}</pre>
+    <!-- <pre>{{ pagedata?.new_home_template.curatedContent }}</pre> -->
     <story-htlAd layout="leaderboard" slotClass="htlad-wnyc_homepage_banner" />
 
     <section v-if="status === 'success'">
