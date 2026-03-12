0289dbfa687901ea150b17b07737edb8de7adda0
Thomas Bono <thomasbono.com@gmail.com>
2026-03-12 17:51:34 -0400

    working on new fetch

--- components/saved/FetchRecentEpisodes.vue
+++ components/saved/FetchRecentEpisodes.vue
@@ -13,19 +13,67 @@ const props = defineProps({
 
 const config = useRuntimeConfig()
 
-const { data, status, error } = useFetch(
-  `${config.public.BFF_URL}/api/show/${props.show.slug}`,
+// const { data, status, error } = useFetch(
+//   `${config.public.BFF_URL}/api/show/${props.show.slug}`,
+//   {
+//     params: {
+//       pageSize: props.episodesPerShow,
+//     },
+//   }
+// )
+const podcastId = ref(null)
+const episodes = ref(null)
+const {
+  data: show,
+  status,
+  error,
+} = useFetch(
+  `${config.public.BFF_URL}/api/pages/wagtail/${props.show.slug}?showOnly=true`,
   {
-    params: {
-      pageSize: props.episodesPerShow,
+    onResponse(res) {
+      podcastId.value = res.response._data.linkedDataSource[0].value.id
     },
+  }
+)
+
+const {
+  data,
+  status: scStatus,
+  error: scError,
+} = useFetch(
+  () =>
+    `${config.public.BFF_URL}/api/v3/show/${
+      podcastId.value
+    }/episodes?offset=0&limit=${props.episodesPerShow || 3}`,
+  {
+    onResponse(res) {
+      //pendingMore.value = false
+      //meta.value = res.response._data.meta
+      //episodes.value = res.response._data.data
+      console.log("res.response._data.data = ", res.response._data.data)
+      episodes.value = res.response._data.data
+    },
+    onError(error) {
+      //pendingMore.value = false
+      const globalToast = useGlobalToast()
+      globalToast.value = {
+        severity: "error",
+        summary:
+          "Sorry. We are having trouble loading more episodes. Please try again later.",
+        life: null,
+        closable: true,
+      }
+      console.error("error = ", error)
+    },
+    watch: [podcastId],
+    immediate: false,
   }
 )
 </script>
 <template>
-  <div v-if="status === 'success'">
+  <div v-if="scStatus === 'success' && episodes">
     <MediaCard
-      v-for="episode in data.episodes.data"
+      v-for="episode in episodes"
       :key="episode.id"
       :data="episode"
       class="my-5"
@@ -53,5 +101,5 @@ const { data, status, error } = useFetch(
       class="my-5"
     />
   </div>
-  <FetchError v-if="error" />
+  <FetchError v-if="scError || error" />
 </template>
