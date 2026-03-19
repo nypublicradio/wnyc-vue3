229ca0fdeb2a5222e6414cdbdbc87552212d29fd
Thomas Bono <thomasbono.com@gmail.com>
2026-03-18 23:48:15 -0400

    cleaned up index and home page loading experience, no more flickering or footer appearing. removed uneeded fields to load on the show page for a quicker load, added path global to fetch

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
 <script setup async>
-import { setStatusDarkMode } from "~/utilities/helpers"
-import { useCurrentUserProfile } from "~/composables/states.ts"
 import { useBrowserTopColorDarkMode } from "~/composables/globals.ts"
 
 useHead({
   bodyAttrs: {
-    class: "no-bottom-padding hide-bottom-menu solid-bg",
+    class: "no-bottom-padding hide-bottom-menu solid-bg hide-footer",
   },
 })
 
@@ -14,31 +12,23 @@ definePageMeta({
   //middleware: ["check-auth-provider"],
 })
 
-const currentUserProfile = useCurrentUserProfile()
 const browserTopColorDarkMode = useBrowserTopColorDarkMode()
 const route = useRoute()
-
-onBeforeMount(() => {
-  // this page has the body class "style-mode-dark", so we need to force the status bar to be dark as well
-  setStatusDarkMode(true)
-})
 
 onMounted(() => {
   setTimeout(() => {
     navigateTo("/home")
   }, 100)
 })
-
-onUnmounted(() => {
-  // check if are set to light mode first, if yes, then set the status bar back to light mode
-  setStatusDarkMode(currentUserProfile.value?.dark_mode)
-})
 </script>
 <template>
-  <div>
+  <div class="index">
     <Html>
       <Head>
-        <Title>WNYC | New York Public Radio, Podcasts, Live Streaming Radio, News</Title>
+        <Title
+          >WNYC | New York Public Radio, Podcasts, Live Streaming Radio,
+          News</Title
+        >
         <Meta
           name="og:title"
           content="WNYC | New York Public Radio, Podcasts, Live Streaming Radio, News"
@@ -49,10 +39,13 @@ onUnmounted(() => {
         />
         <!-- force browser top color dark -->
         <Meta name="theme-color" :content="browserTopColorDarkMode" />
-        <Meta name="msapplication-TileColor" :content="browserTopColorDarkMode" />
+        <Meta
+          name="msapplication-TileColor"
+          :content="browserTopColorDarkMode"
+        />
       </Head>
     </Html>
-    <div class="page style-mode-dark" :class="[`${String(route.name)}`]">
+    <div class="page" :class="[`${String(route.name)}`]">
       <Transition name="fade">
         <section class="loading-holder">
           <WnycLoader class="loader-anim" />
@@ -63,27 +56,29 @@ onUnmounted(() => {
 </template>
 
 <style lang="scss" scoped>
-.loading-holder {
-  display: flex;
-  position: absolute;
-  height: calc(100vh - env(safe-area-inset-top) - env(safe-area-inset-bottom));
-  width: 100%;
-  left: 0;
-  right: 0;
-
-  .loader-anim {
+.index {
+  min-height: 100vh;
+  .loading-holder {
+    display: flex;
     position: absolute;
-    top: 0;
-    bottom: 0;
+    height: calc(
+      100vh - env(safe-area-inset-top) - env(safe-area-inset-bottom)
+    );
+    width: 100%;
     left: 0;
     right: 0;
-    margin: auto;
-    width: 100px;
-    height: 50px;
-  }
-}
+    background-color: #ffffff;
 
-.index-page {
-  height: calc(100vh - env(safe-area-inset-top) - env(safe-area-inset-bottom));
+    .loader-anim {
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      margin: auto;
+      width: 100px;
+      height: 50px;
+    }
+  }
 }
 </style>
