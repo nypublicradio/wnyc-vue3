cfe5ff67d96452df8caede5ca2170131849f93ec
Michael Hearn <git@mikehearn.net>
2026-03-26 10:18:06 -0400

    fix anonymous page view tracking and add analytics coverage tests

--- plugins/firebase.client.js
+++ plugins/firebase.client.js
@@ -2,37 +2,41 @@ import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
 import { Capacitor } from "@capacitor/core"
 
-export default defineNuxtPlugin(async () => {
-    const config = useRuntimeConfig()
+export const buildFirebaseConfig = (platform, publicConfig) => {
+    let appId = null
+    let apiKey = null
 
-    const platform = await Capacitor.getPlatform()
-    let thisAppId = null
-    let thisApiKey = null
     switch (platform) {
         case 'web':
-            thisAppId = config.public.FB_APP_ID_WEB
-            thisApiKey = config.public.FB_API_KEY_WEB
+            appId = publicConfig.FB_APP_ID_WEB
+            apiKey = publicConfig.FB_API_KEY_WEB
             break
         case 'android':
-            thisAppId = config.public.FB_APP_ID_ANDROID
-            thisApiKey = config.public.FB_API_KEY_ANDROID
+            appId = publicConfig.FB_APP_ID_ANDROID
+            apiKey = publicConfig.FB_API_KEY_ANDROID
             break
         case 'ios':
-            thisAppId = config.public.FB_APP_ID_IOS
-            thisApiKey = config.public.FB_API_KEY_IOS
+            appId = publicConfig.FB_APP_ID_IOS
+            apiKey = publicConfig.FB_API_KEY_IOS
             break
         default:
     }
 
-    const firebaseConfig = {
-        apiKey: thisApiKey,
-        authDomain: config.public.FB_AUTH_DOMAIN,
-        projectId: config.public.FB_PROJECT_ID,
-        storageBucket: config.public.FB_STORAGE_BUCKET,
-        messagingSenderId: config.public.FB_MESSAGING_SENDER_ID,
-        appId: thisAppId,
-        measurementId: config.public.FB_MEASUREMENT_ID,
+    return {
+        apiKey,
+        authDomain: publicConfig.FB_AUTH_DOMAIN,
+        projectId: publicConfig.FB_PROJECT_ID,
+        storageBucket: publicConfig.FB_STORAGE_BUCKET,
+        messagingSenderId: publicConfig.FB_MESSAGING_SENDER_ID,
+        appId,
+        measurementId: publicConfig.FB_MEASUREMENT_ID,
     }
+}
+
+export default defineNuxtPlugin(async () => {
+    const config = useRuntimeConfig()
+    const platform = await Capacitor.getPlatform()
+    const firebaseConfig = buildFirebaseConfig(platform, config.public)
     const app = initializeApp(firebaseConfig)
     getAnalytics(app)
 })
