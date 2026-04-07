5e0cb9733546643cf97c4b35d5e0f2a66bce6e70
Thomas Bono <thomasbono.com@gmail.com>
2026-04-07 00:40:34 -0400

    htl ad set to clientOnly and removed a left over stop() function in the show page

--- components/story/HtlAd.vue
+++ components/story/HtlAd.vue
@@ -10,18 +10,20 @@ const sensitiveContent = useSensitiveContent()
 </script>
 
 <template>
-  <div>
-    <div :class="`ad-wrapper ${sensitiveContent ? '' : layout}`">
-      <div v-if="!sensitiveContent" :class="slotClass"></div>
+  <ClientOnly>
+    <div>
+      <div :class="`ad-wrapper ${sensitiveContent ? '' : layout}`">
+        <div v-if="!sensitiveContent" :class="slotClass"></div>
+      </div>
+      <p
+        v-if="fineprint && !sensitiveContent"
+        :class="`type-fineprint text-center ${fineprintClass || ''}`"
+        class="mt-3"
+      >
+        {{ fineprint }}
+      </p>
     </div>
-    <p
-      v-if="fineprint && !sensitiveContent"
-      :class="`type-fineprint text-center ${fineprintClass || ''}`"
-      class="mt-3"
-    >
-      {{ fineprint }}
-    </p>
-  </div>
+  </ClientOnly>
 </template>
 <style lang="scss">
 .ad-wrapper.leaderboard .htl-ad-gpt {
