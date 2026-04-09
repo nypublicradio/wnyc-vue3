d5029ca4c2be786d5ada34343c6b334bac8c634b
Wilfredo Colon <wcolon@nypublicradio.org>
2026-04-09 11:20:42 -0400

    update Sentry dependencies to version 9.5.0 and enhance Sentry initialization in the Nuxt plugin

--- plugins/sentry.client.js
+++ plugins/sentry.client.js
@@ -1,18 +1,53 @@
 import * as Sentry from '@sentry/vue'
-import { HttpClient } from '@sentry/integrations'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  // TEMP: disable Sentry to test if it causes the demo freeze
+  const { vueApp } = nuxtApp
+
+  const config = useRuntimeConfig()
+
+  Sentry.init({
+    app: [vueApp],
+    dsn: config.public.SENTRY_DSN,
+    integrations: [
+      Sentry.browserTracingIntegration({
+        router: nuxtApp.$router,
+        enableInp: true,
+        interactionsSampleRate: 0.5,
+      }),
+    ],
+    tracesSampleRate: config.public.SENTRY_ENV.toUpperCase() === 'PROD' ? 0.1 : 1.0,
+    allowUrls: [
+      'https://wnyc.org',
+      'https://www.wnyc.org',
+      'https://prod.wnyc.org',
+      'https://demo.wnyc.org',
+      'https://native-app.wnyc.org',
+      'https://demo.native-app.wnyc.org',
+    ],
+    ignoreErrors: [
+      'ResizeObserver loop limit exceeded',
+      'Failed to fetch',
+      'NetworkError when attempting to fetch resource.',
+      'Load failed',
+    ],
+    maxValueLength: 1000,
+    trackComponents: true,
+    timeout: 2000,
+    hooks: ['activate', 'mount', 'update'],
+    logErrors: true,
+    debug: false,
+    environment: config.public.SENTRY_ENV,
+  })
+
   return {
     provide: {
       sentry: {
-        setContext: () => { },
-        setUser: () => { },
-        setTag: () => { },
-        addBreadcrumb: () => { },
-        captureException: () => { },
+        setContext: (n, context) => Sentry.setContext(n, context),
+        setUser: user => Sentry.setUser(user),
+        setTag: (tagName, value) => Sentry.setTag(tagName, value),
+        addBreadcrumb: breadcrumb => Sentry.addBreadcrumb(breadcrumb),
+        captureException: exception => Sentry.captureException(exception),
       },
     },
   }
-
 })
