3047380d03d5af9d7379b6893f6acdf70bb94032
Thomas Bono <thomasbono.com@gmail.com>
2026-04-14 17:39:25 -0400

    removed crawlLinks... that is causing the odd situation where you reload on a show page and every route after that failes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -66,7 +66,7 @@ export default defineNuxtConfig({
     },
     prerender: {
       // Disable prerendering when SSR is false (SPA mode for mobile)
-      crawlLinks: process.env.NUXT_SSR === 'true',
+      //crawlLinks: process.env.NUXT_SSR === 'true',
       //routes: process.env.NUXT_SSR === 'true' ? ['/'] : ['/'],
       // Don't fail the build on prerender errors for client-only routes
       failOnError: false,
