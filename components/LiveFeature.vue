c5ee993c7ca0d66e66befd6c6957c08b384df78d
Thomas Bono <thomasbono.com@gmail.com>
2026-04-17 18:04:31 -0400

    added format and rounding function, used it on the schedule and used that on the live feature as well

--- components/LiveFeature.vue
+++ components/LiveFeature.vue
@@ -19,7 +19,7 @@ import useLiveStream, {
   updateLiveStream,
   updateAllLiveStreams,
 } from "~/composables/data/liveStream"
-const { getStationBySlugAndPlayIt } = useLiveStream()
+const { getStationBySlugAndPlayIt, formatAndRoundTime } = useLiveStream()
 const currentEpisodeHolder = useCurrentEpisodeHolder()
 const isApp = useIsApp()
 const togglePlayTrigger = useTogglePlayTrigger()
@@ -57,7 +57,6 @@ const onUpdateStation = (station) => {
 <template>
   <div class="live-feature">
     <div class="holder overflow-hidden">
-      <!-- <VFlexibleLink raw to="/live"> -->
       <VFlexibleLink
         raw
         to="/live"
@@ -78,13 +77,7 @@ const onUpdateStation = (station) => {
               :key="currentEpisodeHolder?.id"
               loading="eager"
             />
-            <WnycLoader
-              v-else
-              class="image-loader-anim"
-              size="1rem"
-              bg
-              spinner
-            />
+            <WnycLoader v-else class="image-loader-anim" size="1rem" bg spinner />
           </Transition>
         </div>
         <div class="content w-full relative">
@@ -111,20 +104,15 @@ const onUpdateStation = (station) => {
                   |
                 </p>
                 <p
-                  v-if="
-                    currentEpisodeHolder.timeStart &&
-                    currentEpisodeHolder.timeEnd
-                  "
+                  v-if="currentEpisodeHolder.timeStart && currentEpisodeHolder.timeEnd"
                   class="font-bold"
                 >
-                  {{ formatTime(currentEpisodeHolder.timeStart, "h:mm a") }} -
-                  {{ formatTime(currentEpisodeHolder.timeEnd, "h:mm a") }}
+                  {{ formatAndRoundTime(currentEpisodeHolder.timeStart) }} -
+                  {{ formatAndRoundTime(currentEpisodeHolder.timeEnd) }}
                 </p>
               </div>
             </div>
-            <h2
-              class="md:text-xl lg:text-2xl xl:text-4xl line-height-2 truncate t2lines"
-            >
+            <h2 class="md:text-xl lg:text-2xl xl:text-4xl line-height-2 truncate t2lines">
               {{ currentEpisodeHolder?.title }}
             </h2>
             <div
@@ -138,9 +126,7 @@ const onUpdateStation = (station) => {
                 currentEpisodeHolder?.details
               "
             ></div>
-            <div
-              class="flex align-items-start justify-content-start mt-1 gap-0 md:gap-2"
-            >
+            <div class="flex align-items-start justify-content-start mt-1 gap-0 md:gap-2">
               <div class="flex flex-row gap-3 flex-wrap md:flex-column">
                 <PlayButton
                   :label="
@@ -156,19 +142,14 @@ const onUpdateStation = (station) => {
                   live
                   class="z-2"
                 />
-                <div
-                  v-if="!isApp"
-                  class="hidden md:flex flex-row gap-3 flex-wrap"
-                >
+                <div v-if="!isApp" class="hidden md:flex flex-row gap-3 flex-wrap">
                   <Button
                     label="Get the App"
                     severity="secondary"
                     class="p-button-sm flex w-10rem justify-content-start h-2rem p-button-center-label-with-icon z-2"
                     @click.prevent="
                       navigateTo(appDownloadLink, {
-                        external: appDownloadLink.startsWith('http')
-                          ? true
-                          : false,
+                        external: appDownloadLink.startsWith('http') ? true : false,
                       })
                     "
                   >
@@ -253,24 +234,10 @@ const onUpdateStation = (station) => {
                 borderRadius="16px"
               />
             </div>
-            <Skeleton
-              class="w-9rem md:w-21rem"
-              height="28px"
-              borderRadius="16px"
-            />
+            <Skeleton class="w-9rem md:w-21rem" height="28px" borderRadius="16px" />
             <div class="hidden md:flex flex-row gap-3 mt-1 lg:-mt-1">
-              <Skeleton
-                class="w-10rem"
-                height="28px"
-                width="9rem"
-                borderRadius="16px"
-              />
-              <Skeleton
-                class="w-10rem"
-                height="28px"
-                width="9rem"
-                borderRadius="16px"
-              />
+              <Skeleton class="w-10rem" height="28px" width="9rem" borderRadius="16px" />
+              <Skeleton class="w-10rem" height="28px" width="9rem" borderRadius="16px" />
             </div>
           </div>
         </div>
