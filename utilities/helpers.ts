4d3ed1fa066f50647ec4e042f5964208eebfedff
Thomas Bono <thomasbono.com@gmail.com>
2026-04-18 01:18:38 -0400

    looks good, ready for code review

--- utilities/helpers.ts
+++ utilities/helpers.ts
@@ -461,13 +461,11 @@ export const shareAPI = async (
   content,
   componentOfOrigin = "Component of origin not specified"
 ) => {
-  console.log('content', content)
   const shareData = {
     title: stripHtmlTags(content.socialTitle || content.title),
     text: stripHtmlTags(content.rawBody || content.description || content.title),
     url: content.shareUrl || content.url || content.titleLink,
   }
-
   trackClickEvent("Click Tracking - Share", componentOfOrigin, shareData.title)
   // Native Mobile Sharing
   if (Capacitor.isNativePlatform()) {
