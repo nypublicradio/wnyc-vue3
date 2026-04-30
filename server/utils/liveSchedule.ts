868a45d07695b84ae1fec90a4af691ac61772173
Thomas Bono <thomasbono.com@gmail.com>
2026-04-30 11:00:06 -0400

    added new PDF link

--- server/utils/liveSchedule.ts
+++ server/utils/liveSchedule.ts
@@ -24,7 +24,7 @@ function hasValidTimes (episode: ScheduleEpisode) {
 }
 // easy BFF location to store the link to the schedule PDF
 export function getSchedulePdfLink () {
-    return "https://media.wnyc.org/media/resources/2025/Mar/31/wnyc-schedule.pdf"
+    return "https://media.wnyc.org/media/resources/2026/Apr/29/wnyc-schedule.pdf"
 }
 
 export function getCurrentEpisodeSelectionFromSchedule (
