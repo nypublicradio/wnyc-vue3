96fe5bc9fab3056a97190c9d9bdfb326e702123e
Thomas Bono <thomasbono.com@gmail.com>
2026-05-05 16:56:36 -0400

    clean up

--- composables/useTopStories.ts
+++ composables/useTopStories.ts
@@ -6,34 +6,8 @@ interface Article {
 export const useTopStories = () => {
   const config = useRuntimeConfig()
   const toast = useToast()
-  const API_TIMEOUT = 5000
-  // const options = {
-  //   method: 'GET',
-  //   url: `${config.public.AVIARY_BASE_API}curated_lists/4`,
-  //   headers: {
-  //     'X-CMS-Site': config.public.cmsSite
-  //   },
-  //   timeout: API_TIMEOUT,
-  // }
 
-  //const topStoriesRes = await axios(options)
-
-  const { data: topStoriesData } = useFetchWrapper('/api/curated_lists/4')
-
-  // const curationFetchArgs = [
-  //   `${config.public.BFF_URL}/api/homepagecuration`,
-  //   {
-  //     key: "home-page-curation",
-  //     retry: 2,
-  //     retryDelay: 500,
-  //   },
-  // ]
-
-  // const {
-  //   data: topStoriesData,
-  //   status,
-  //   error,
-  // } = useFetchWrapper(...topStoriesList)
+  const { data: topStoriesData, status, error } = useFetchWrapper('/api/curated_lists/4')
 
   // Computed property to safely extract top_stories array
   const topStories = computed(() => {
@@ -49,25 +23,25 @@ export const useTopStories = () => {
   }
 
   // Watch for errors and show toast
-  // watch(error, (newError) => {
-  //   if (newError) {
-  //     console.error("Error fetching top stories:", newError)
-  //     toast.add({
-  //       severity: "error",
-  //       summary:
-  //         "We are having a problem loading the top stories. Please try again later.",
-  //       life: 6000,
-  //       closable: true,
-  //     })
-  //   }
-  // })
+  watch(error, (newError) => {
+    if (newError) {
+      console.error("Error fetching top stories:", newError)
+      toast.add({
+        severity: "error",
+        summary:
+          "We are having a problem loading the top stories. Please try again later.",
+        life: 6000,
+        closable: true,
+      })
+    }
+  })
 
   return {
     topStories,
     getFilteredTopStories,
     topStoriesData,
-    //error,
-    //status,
-    //isLoading: computed(() => status.value === 'pending'),
+    error,
+    status,
+    isLoading: computed(() => status.value === 'pending'),
   }
 }
