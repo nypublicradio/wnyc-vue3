ff05c7cb6f16bb5cc7cba89c136f1cefb45cabc4
Thomas Bono <thomasbono.com@gmail.com>
2026-09-19 19:08:23 -0400

    copied over code from old branch

--- components/VStreamfield.vue
+++ components/VStreamfield.vue
@@ -291,11 +291,24 @@ onMounted(() => {
       <div v-if="block.type === 'image'" class="streamfield-image mt-4 mx-auto">
         <VImage
           :src="block.value.image"
-          :ratio="[block.value.image.width ?? 3, block.value.image.height ?? 2]"
           :alt="block.value.image.alt"
           :maxWidth="block.value.image.width"
           :maxHeight="block.value.image.height"
-          sizes="xs:390px md:768px"
+          :object-fit="
+            block.value.image.objectFit === 'fit' ? 'contain' : 'cover'
+          "
+          :ratio="
+            block.value.image.objectFit === 'fit'
+              ? [16, 9]
+              : [block.value.image.width ?? 16, block.value.image.height ?? 9]
+          "
+          :size="{
+            xxs: [317],
+            xs: [517],
+            sm: [672],
+            md: [885],
+            lg: [662],
+          }"
         >
           <!--           <template #caption>
             <VImageCaption
